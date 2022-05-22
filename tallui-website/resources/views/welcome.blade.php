8bda64a3a738dfc40b2d150abc0995d9b57c7e79
Alf Drollinger <alf@drollinger.info>
2022-05-22 10:17:57 +0200

    Install Jetstream as core dep

--- tallui-website/resources/views/welcome.blade.php
+++ tallui-website/resources/views/welcome.blade.php
@@ -36,6 +36,10 @@
                 </div>
             @endif
 
+            <div>
+                @livewire('first-component')
+            </div>
+
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
                     <svg viewBox="0 0 651 192" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-16 w-auto text-gray-700 sm:h-20">
