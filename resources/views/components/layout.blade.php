9015b22517a4d9c499aee555ab2c329cce214cfe
Alf Drollinger <40421928+adrolli@users.noreply.github.com>
2022-06-09 11:12:45 +0200

    Icon-search w-i-p

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,10 +1,6 @@
 <x-html class="font-sans border-t-8 border-scarlet-500" :title="isset($title) ? $title . ' - ' . config('app.name') : ''">
     <x-slot name="head">
-        <x-social-meta
-            :title="$component->title()"
-            description="A set of renderless components to utilise in your Laravel Blade views. Built for the TALL stack. Completely open-source."
-            :image="asset('/images/socialcard.png')"
-        />
+
 
         <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('/apple-touch-icon.png') }}">
         <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('/favicon-32x32.png') }}">
@@ -22,11 +18,7 @@
         @livewireStyles
         @bukStyles
 
-        @production
-            <!-- Fathom - beautiful, simple website analytics -->
-            <script src="https://boom.laravel.io/script.js" site="SVSHYYIX" defer></script>
-            <!-- / Fathom -->
-        @endproduction
+
     </x-slot>
 
     {{ $slot }}
