3bb28c7effa7d5116e3dbcc8b052ebfd65f266c0
Alf Drollinger <alf@drollinger.info>
2022-10-16 23:14:46 +0200

    Update TestCase.php

--- _packages/tallui-core/tests/TestCase.php
+++ _packages/tallui-core/tests/TestCase.php
@@ -28,7 +28,7 @@ class TestCase extends Orchestra
     {
         config()->set('database.default', 'testing');
 
-        $migration = include __DIR__.'/../database/migrations/create_tallui-core_table.php.stub';
+        $migration = include __DIR__.'/../database/migrations/create_tallui_core_table.php.stub';
         $migration->up();
     }
 }
