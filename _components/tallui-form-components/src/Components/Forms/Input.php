e4a44ccebbcbf9db01b22c340a748eec95f8b28c
Alf Drollinger <alf@drollinger.info>
2022-10-17 00:48:13 +0200

    Update Input.php

--- _components/tallui-form-components/src/Components/Forms/Input.php
+++ _components/tallui-form-components/src/Components/Forms/Input.php
@@ -20,7 +20,7 @@ class Input extends Component
     /** @var string */
     public $value;
 
-    public function __construct(string $name, string $id = null, string $type = null, string $value)
+    public function __construct(string $name, string $id = null, string $type = null, string $value = null)
     {
         $this->name = $name;
         $this->id = $id ?? $name;
