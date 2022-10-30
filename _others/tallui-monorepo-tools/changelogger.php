0f717c2b90aee12184551e7a3821d6a1e65361ba
adrolli <adrolli@users.noreply.github.com>
2022-10-30 10:11:57 +0000

    Fix styling

--- _others/tallui-monorepo-tools/changelogger.php
+++ _others/tallui-monorepo-tools/changelogger.php
@@ -8,15 +8,14 @@
  * returns array $packages
  *
  * */
-
 $lines = file('../../CHANGELOG.md');
 
 $current = getcurrent($lines);
 $find = array_search($current, $lines);
 $lines = array_values(array_slice($lines, $find + 1, null, true));
 
-list($version, $date) = version_date($current);
-echo $version . " at " . $date . "<br><br>";
+[$version, $date] = version_date($current);
+echo $version.' at '.$date.'<br><br>';
 
 $current = getcurrent($lines);
 $find = array_search($current, $lines);
@@ -25,11 +24,11 @@ $lines = array_slice($lines, 0, $find - 1, true);
 $packages = find_packages($lines);
 
 if ($packages) {
-    echo "Packages:<br><ul>";
-    foreach($packages as $package) {
-        echo "<li>" . $package . "</li>";
+    echo 'Packages:<br><ul>';
+    foreach ($packages as $package) {
+        echo '<li>'.$package.'</li>';
     }
-    echo "</ul>";
+    echo '</ul>';
 }
 
 function find_packages($lines)
@@ -39,15 +38,17 @@ function find_packages($lines)
             $packages[] = trim($line, '### ');
         }
     }
+
     return $packages;
 }
 
 function version_date($current)
 {
-    $version_date = explode(" @ ", $current);
+    $version_date = explode(' @ ', $current);
     $version = trim($version_date[0], '## v');
     $date = $version_date[1];
-    return array($version, $date);
+
+    return [$version, $date];
 }
 
 function getcurrent($lines)
