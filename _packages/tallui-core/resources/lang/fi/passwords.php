5bf17fedb26548266afa129ba387c45f022594bc
J. Lavoie <j.lavoie@net-c.ca>
2022-11-03 03:21:03 +0000

    Translated using Weblate (Finnish)
    Currently translated at 100.0% (5 of 5 strings)
    
    Translation: TallUI/TallUI Core Passwords
    Translate-URL: https://hosted.weblate.org/projects/tallui/tallui-core-passwords/fi/

--- _packages/tallui-core/resources/lang/fi/passwords.php
+++ _packages/tallui-core/resources/lang/fi/passwords.php
@@ -1,11 +1,8 @@
 <?php
-
-declare(strict_types=1);
-
 return [
-    'reset' => 'Salasana on resetoitu!',
-    'sent' => 'Resetointilinkki lähetetty sähköpostitse!',
-    'throttled' => 'Odota, ennen kuin yrität uudelleen',
+    'reset' => 'Salasanasi on palautettu!',
+    'sent' => 'Palautuslinkki lähetetty sähköpostitse!',
+    'throttled' => 'Odota, ennen kuin yrität uudelleen.',
     'token' => 'Resetointitunniste on viallinen.',
     'user' => 'Sähköpostiosoitteella ei löydy käyttäjää.',
 ];
