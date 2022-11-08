c764897a3595c0f767ea876fab1ce089c5af9519
Alf Drollinger <40421928+adrolli@users.noreply.github.com>
2022-11-09 00:17:48 +0100

    New translations auth.php (German)

--- _packages/tallui-core/resources/lang/de/auth.php
+++ _packages/tallui-core/resources/lang/de/auth.php
@@ -5,5 +5,5 @@ declare(strict_types=1);
 return [
     'failed' => 'Diese Kombination aus Zugangsdaten wurde nicht in unserer Datenbank gefunden.',
     'password' => 'Das Passwort ist falsch.',
-    'throttle' => 'Zu viele Loginversuche. Versuchen Sie es bitte in :seconds Sekunden nochmal.',
+    'throttle' => 'Zu viele Login-Versuche. Bitte versuchen Sie es in :seconds Sekunden erneut.',
 ];
