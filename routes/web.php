be2e7cbc6a10164b6d72d306ed2b188eb3f797cb
KimSpeer <kim.speer@co-IT.eu>
2022-11-09 16:17:57 +0100

    WIP Welcome view TallUI

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/custom', function () {
+    return view('test');
+});
+
+
 
 Route::middleware([
     'auth:sanctum',
