70f720bd72bbf5a6947a4356db45719347491aef
KimSpeer <kim.speer@co-IT.eu>
2022-11-11 15:20:43 +0100

    WIP Custom Views

---
+++ resources/views/components/directory-digger.blade.php
@@ -0,0 +1,3 @@
+<div>
+    Hellou There Im a Digger
+</div>
--- resources/views/custom/custom_alf.blade.php
+++ resources/views/custom/custom_alf.blade.php
@@ -1,27 +1,6 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>TallUI is coming soon</title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+@extends('layouts.pages')
 
-
-        <!-- Fonts -->
-        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-
-        <!-- Styles -->
-        <style>
-
-        </style>
-
-        <style>
-            body {
-                font-family: 'Nunito', sans-serif;
-            }
-        </style>
-    </head>
-    <body class="bg-[#001829] bg-[url('../../public//build/assets/images/bg.jpg')] bg-no-repeat bg-right-top text-[#0e9adc]">
+@section('content')
         <head>
             <nav class="flex justify-between m-8">
                 <img  src="/build/assets/images/logo.png">
@@ -47,6 +26,4 @@
                 Our first components will be available shortly.
             </p>
         </div>
-
-    </body>
-</html>
+@stop
