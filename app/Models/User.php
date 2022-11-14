575a197022feee1c503de687b04a16d99c39b15e
KimSpeer <kim.speer@co-IT.eu>
2022-11-14 08:58:44 +0100

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -43,7 +43,7 @@ class User extends Authenticatable
     /**
      * The attributes that should be cast.
      *
-     * @var array
+     * @var array<mixed>
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
