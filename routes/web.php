7f96b66092f7a16e4480b4f56de1d2611e7f913e
adrolli <adrolli@users.noreply.github.com>
2022-11-17 00:17:31 +0000

    Fix styling

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-     return view('welcome');
+    return view('welcome');
 });
 
 Route::get('/custom/alf', function () {
