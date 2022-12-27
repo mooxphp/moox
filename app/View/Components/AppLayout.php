2cb2a115225aa74b56c32d8e8502dffac1ff749b
Alf Drollinger <40421928+adrolli@users.noreply.github.com>
2022-12-27 01:46:11 +0100

    Fix PHPStan

--- app/View/Components/AppLayout.php
+++ app/View/Components/AppLayout.php
@@ -6,11 +6,6 @@ use Illuminate\View\Component;
 
 class AppLayout extends Component
 {
-    /**
-     * Get the view / contents that represents the component.
-     *
-     * @return Illuminate\Contracts\View\View
-     */
     public function render()
     {
         return view('layouts.app');
--- app/View/Components/GuestLayout.php
+++ app/View/Components/GuestLayout.php
@@ -6,11 +6,6 @@ use Illuminate\View\Component;
 
 class GuestLayout extends Component
 {
-    /**
-     * Get the view / contents that represents the component.
-     *
-     * @return Illuminate\Contracts\View\View
-     */
     public function render()
     {
         return view('layouts.guest');
