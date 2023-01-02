a395c0196a64be2b220851f8255b619c8b2a7b84
adrolli <adrolli@users.noreply.github.com>
2023-01-02 15:35:11 +0000

    Fix styling

--- app/Http/Livewire/UserTable.php
+++ app/Http/Livewire/UserTable.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\User;
 use Rappasoft\LaravelLivewireTables\DataTableComponent;
 use Rappasoft\LaravelLivewireTables\Views\Column;
-use App\Models\User;
 
 class UserTable extends DataTableComponent
 {
@@ -18,15 +18,15 @@ class UserTable extends DataTableComponent
     public function columns(): array
     {
         return [
-            Column::make("Id", "id")
+            Column::make('Id', 'id')
                 ->sortable(),
-            Column::make("Name", "name")
+            Column::make('Name', 'name')
                 ->sortable(),
-            Column::make("Email", "email")
+            Column::make('Email', 'email')
                 ->sortable(),
-            Column::make("Created at", "created_at")
+            Column::make('Created at', 'created_at')
                 ->sortable(),
-            Column::make("Updated at", "updated_at")
+            Column::make('Updated at', 'updated_at')
                 ->sortable(),
         ];
     }
