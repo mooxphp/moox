371e1c3fc65a2325bcc3990b7c15ade1f230ee4b
Alf Drollinger <40421928+adrolli@users.noreply.github.com>
2023-01-02 16:37:33 +0100

    Revert "Wip(AdminPanel): Testing Laravel Livewire Datatable"
    This reverts commit 000eeefa94d22a0fa766751a9e108e0c00a3e7e3.

--- _packages/tallui-admin-panel/resources/views/adminPanel.blade.php
+++ _packages/tallui-admin-panel/resources/views/adminPanel.blade.php
@@ -352,10 +352,6 @@
                                     user</button>
                             </div>
                         </div>
-
-                        <livewire:user-table />
-
-
                         <div class="flex flex-col mt-8">
                             <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                                 <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
