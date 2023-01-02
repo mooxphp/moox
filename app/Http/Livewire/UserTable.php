000eeefa94d22a0fa766751a9e108e0c00a3e7e3
Alf Drollinger <40421928+adrolli@users.noreply.github.com>
2023-01-02 16:34:25 +0100

    Wip(AdminPanel): Testing Laravel Livewire Datatable

---
+++ app/Http/Livewire/UserTable.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Rappasoft\LaravelLivewireTables\DataTableComponent;
+use Rappasoft\LaravelLivewireTables\Views\Column;
+use App\Models\User;
+
+class UserTable extends DataTableComponent
+{
+    protected $model = User::class;
+
+    public function configure(): void
+    {
+        $this->setPrimaryKey('id');
+    }
+
+    public function columns(): array
+    {
+        return [
+            Column::make("Id", "id")
+                ->sortable(),
+            Column::make("Name", "name")
+                ->sortable(),
+            Column::make("Email", "email")
+                ->sortable(),
+            Column::make("Created at", "created_at")
+                ->sortable(),
+            Column::make("Updated at", "updated_at")
+                ->sortable(),
+        ];
+    }
+}
