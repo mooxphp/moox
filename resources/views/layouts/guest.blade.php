b7478172066bcd7ae82a4bc95d32f125b32c6ce2
Alf Drollinger <40421928+adrolli@users.noreply.github.com>
2023-01-08 02:18:10 +0100

    Wip(Monorepo): Wired Admin-Panel

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,39 +1,80 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>TallUI Monorepo - Dev App</title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=exo-2:400,600,800" rel="stylesheet" />
+<head>
+    <title>TallUI Monorepo - Dev App</title>
+    <meta charset="utf-8">
+    <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0"
+    >
 
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="bg-[#001829] bg-no-repeat bg-right-top text-[#0e9bdc]" style="background-image: url('{{ asset('img/bg.jpg') }}')">
-        <header>
-            <nav class="flex justify-between m-8">
-                <img  src="{{ asset('img/logo.png') }}">
-                <div class="flex items-center text-3xl">
-                    <a class="px-4 text-lg hover:text-[#69bce2]" href="/">Home</a>
-                    <a class="px-4 text-lg hover:text-[#69bce2]" href="/packages">Packages</a>
-                    <a class="px-4 text-lg hover:text-[#69bce2]" href="/components">Components</a>
-                    <a class="px-4 text-lg hover:text-[#69bce2]" href="/custom">Custom</a>
-                    <a class="px-4 text-lg hover:text-[#69bce2]" href="/dashboard">Admin</a>
-                    <a class="has-tooltip" href="https://github.com/usetall/tallui">
-                        <img class="h-8 px-4" style="filter: drop-shadow(0px 3px 3px #0C9ADC);" src="{{ asset('img/octocat.png') }} " alt="Code on GitHub">
-                        <span class='text-xs ml-3 tooltip p-1 rounded bg-sky-900'>GitHub</span>
-                    </a>
-                </div>
-            </nav>
-        </header>
+    <!-- Fonts -->
+    <link
+        rel="preconnect"
+        href="https://fonts.bunny.net"
+    >
+    <link
+        href="https://fonts.bunny.net/css?family=exo-2:400,600,800"
+        rel="stylesheet"
+    />
 
-        <div id="content" class="mb-10">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
 
-            @yield('content')
+<body
+    class="bg-[#001829] bg-no-repeat bg-right-top text-[#0e9bdc]"
+    style="background-image: url('{{ asset('img/bg.jpg') }}')"
+>
+    <header>
+        <nav class="flex justify-between m-8">
+            <img src="{{ asset('img/logo.png') }}">
+            <div class="flex items-center text-3xl">
+                <a
+                    class="px-4 text-lg hover:text-[#69bce2]"
+                    href="/"
+                >Home</a>
+                <a
+                    class="px-4 text-lg hover:text-[#69bce2]"
+                    href="/packages"
+                >Packages</a>
+                <a
+                    class="px-4 text-lg hover:text-[#69bce2]"
+                    href="/components"
+                >Components</a>
+                <a
+                    class="px-4 text-lg hover:text-[#69bce2]"
+                    href="/custom"
+                >Custom</a>
+                <a
+                    class="px-4 text-lg hover:text-[#69bce2]"
+                    href="/admin"
+                >Admin</a>
+                <a
+                    class="has-tooltip"
+                    href="https://github.com/usetall/tallui"
+                >
+                    <img
+                        class="h-8 px-4"
+                        style="filter: drop-shadow(0px 3px 3px #0C9ADC);"
+                        src="{{ asset('img/octocat.png') }} "
+                        alt="Code on GitHub"
+                    >
+                    <span class='text-xs ml-3 tooltip p-1 rounded bg-sky-900'>GitHub</span>
+                </a>
+            </div>
+        </nav>
+    </header>
 
-        </div>
+    <div
+        id="content"
+        class="mb-10"
+    >
 
-    </body>
+        @yield('content')
+
+    </div>
+
+</body>
+
 </html>
