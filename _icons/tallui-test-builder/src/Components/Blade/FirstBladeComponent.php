038841586b566b41afa5fe4dafb3f57f84f189cb
Alf Drollinger <40421928+adrolli@users.noreply.github.com>
2023-01-10 19:34:32 +0100

    Wip(Monorepo): Test and compare both builders

---
+++ _icons/tallui-test-builder/src/Components/Blade/FirstBladeComponent.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace VendorName\Skeleton\Components\Blade;
+
+use Illuminate\Contracts\View\View;
+use VendorName\Skeleton\Components\BladeComponent;
+
+class FirstBladeComponent extends BladeComponent
+{
+    /** @var array<mixed> */
+    protected static $assets = ['example'];
+
+    public string $first_var = '';
+
+    public function mount(): void
+    {
+        // mount
+    }
+
+    public function render(): View
+    {
+        return view(':builder::components.blade.first-blade-component');
+    }
+}
---
+++ _icons/tallui-test-builder/src/Components/Livewire/FirstLivewireComponent.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace VendorName\Skeleton\Components\Livewire;
+
+use Illuminate\Contracts\View\View;
+use VendorName\Skeleton\Components\LivewireComponent;
+
+class FirstLivewireComponent extends LivewireComponent
+{
+    /** @var array<mixed> */
+    protected static $assets = ['example'];
+
+    public string $first_var = '';
+
+    public function mount(): void
+    {
+        // mount
+    }
+
+    public function render(): View
+    {
+        return view(':builder::components.livewire.first-livewire-component');
+    }
+}
