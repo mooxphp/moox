303595871f288ccf74133bc306db78066617eb9f
KimSpeer <kim.speer@co-IT.eu>
2023-02-09 14:23:11 +0100

    fix(pint):run pint

--- _builder/tallui-icons-builder/src/TallUIIconsBuilderServiceProvider.php
+++ _builder/tallui-icons-builder/src/TallUIIconsBuilderServiceProvider.php
@@ -17,24 +17,24 @@ final class TallUIIconsBuilderServiceProvider extends ServiceProvider
         $this->callAfterResolving(Factory::class, function (Factory $factory, Container $container) {
             $config = $container->make('config')->get('tallui-icons-builder', []);
 
-            $factory->add('tallui-icons-builder', array_merge(['path' => __DIR__ . '/../resources/svg'], $config));
+            $factory->add('tallui-icons-builder', array_merge(['path' => __DIR__.'/../resources/svg'], $config));
         });
     }
 
     private function registerConfig(): void
     {
-        $this->mergeConfigFrom(__DIR__ . '/../config/tallui-icons-builder.php', 'tallui-icons-builder');
+        $this->mergeConfigFrom(__DIR__.'/../config/tallui-icons-builder.php', 'tallui-icons-builder');
     }
 
     public function boot(): void
     {
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__ . '/../resources/svg' => public_path('vendor/tallui-icons-builder'),
+                __DIR__.'/../resources/svg' => public_path('vendor/tallui-icons-builder'),
             ], 'tallui-icons-builder');
 
             $this->publishes([
-                __DIR__ . '/../config/tallui-icons-builder.php' => $this->app->configPath('tallui-icons-builder.php'),
+                __DIR__.'/../config/tallui-icons-builder.php' => $this->app->configPath('tallui-icons-builder.php'),
             ], 'tallui-icons-builder-config');
         }
     }
--- _builder/tallui-icons-builder/tests/CompilesIconsTest.php
+++ _builder/tallui-icons-builder/tests/CompilesIconsTest.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace Tests;
 
-use TallUIIconsBuilder\TallUIIconsBuilderServiceProvider;
 use Orchestra\Testbench\TestCase;
+use TallUIIconsBuilder\TallUIIconsBuilderServiceProvider;
 
 class CompilesIconsTest extends TestCase
 {
