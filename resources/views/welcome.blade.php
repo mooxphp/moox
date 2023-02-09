c68790c76aab76b87c9c41cc8765552737e4ada2
KimSpeer <kim.speer@co-IT.eu>
2023-02-09 13:49:59 +0100

    fix(icons-builder):changed return Statements

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,76 +14,117 @@
         </p>
 
         <div class="flex justify-center m-5">
-            <a href="https://www.tallui.io"
-                class="hover:text-[#69bce2] mx-5 px-5 py-2 border-[#002945] bg-[#002945] text-2xl font-extrabold border-2 rounded">tallui.io</a>
+            <a
+                href="https://www.tallui.io"
+                class="hover:text-[#69bce2] mx-5 px-5 py-2 border-[#002945] bg-[#002945] text-2xl font-extrabold border-2 rounded"
+            >tallui.io</a>
         </div>
     </div>
+
     <footer class="mt-105">
         <div class="flex flex-row items-center justify-center gap-3 mt-3">
 
             <a href="https://github.com/usetall/tallui/actions/workflows/pest.yml">
-                <img alt="PEST Tests" src="https://github.com/usetall/tallui/actions/workflows/pest.yml/badge.svg">
+                <img
+                    alt="PEST Tests"
+                    src="https://github.com/usetall/tallui/actions/workflows/pest.yml/badge.svg"
+                >
             </a>
 
             <a href="https://github.com/usetall/tallui/actions/workflows/pint.yml">
-                <img alt="Laravel PINT PHP Code Style"
-                    src="https://github.com/usetall/tallui/actions/workflows/pint.yml/badge.svg">
+                <img
+                    alt="Laravel PINT PHP Code Style"
+                    src="https://github.com/usetall/tallui/actions/workflows/pint.yml/badge.svg"
+                >
             </a>
 
             <a href="https://github.com/usetall/tallui/actions/workflows/phpstan.yml">
-                <img alt="PHPStan Level 5" src="https://github.com/usetall/tallui/actions/workflows/phpstan.yml/badge.svg">
+                <img
+                    alt="PHPStan Level 5"
+                    src="https://github.com/usetall/tallui/actions/workflows/phpstan.yml/badge.svg"
+                >
             </a>
 
         </div>
         <div class="flex flex-row items-center justify-center gap-3 mt-3">
 
             <a href="https://www.tailwindcss.com">
-                <img alt="TailwindCSS 3"
-                    src="https://img.shields.io/badge/TailwindCSS-v3-orange?logo=tailwindcss&color=06B6D4">
+                <img
+                    alt="TailwindCSS 3"
+                    src="https://img.shields.io/badge/TailwindCSS-v3-orange?logo=tailwindcss&color=06B6D4"
+                >
             </a>
             <a href="https://www.alpinejs.dev">
-                <img alt="AlpineJS 3" src="https://img.shields.io/badge/AlpineJS-v3-orange?logo=alpine.js&color=8BC0D0">
+                <img
+                    alt="AlpineJS 3"
+                    src="https://img.shields.io/badge/AlpineJS-v3-orange?logo=alpine.js&color=8BC0D0"
+                >
             </a>
             <a href="https://www.laravel.com">
-                <img alt="Laravel 9" src="https://img.shields.io/badge/Laravel-v9-orange?logo=Laravel&color=FF2D20">
+                <img
+                    alt="Laravel 9"
+                    src="https://img.shields.io/badge/Laravel-v9-orange?logo=Laravel&color=FF2D20"
+                >
             </a>
             <a href="https://www.laravel-livewire.com">
-                <img alt="Laravel Livewire 2"
-                    src="https://img.shields.io/badge/Livewire-v2-orange?logo=livewire&color=4E56A6">
+                <img
+                    alt="Laravel Livewire 2"
+                    src="https://img.shields.io/badge/Livewire-v2-orange?logo=livewire&color=4E56A6"
+                >
             </a>
         </div>
         <div class="flex flex-row items-center justify-center gap-3 mt-3">
 
             <a href="https://app.codacy.com/gh/usetall/tallui/dashboard">
-                <img src="https://app.codacy.com/project/badge/Grade/2b912412bb6e4892b52688272dec1555"
-                    alt="Codacy Code Quality">
+                <img
+                    src="https://app.codacy.com/project/badge/Grade/2b912412bb6e4892b52688272dec1555"
+                    alt="Codacy Code Quality"
+                >
             </a>
             <a href="https://app.codacy.com/gh/usetall/tallui/dashboard">
-                <img src="https://app.codacy.com/project/badge/Coverage/2b912412bb6e4892b52688272dec1555"
-                    alt="Codacy Coverage">
+                <img
+                    src="https://app.codacy.com/project/badge/Coverage/2b912412bb6e4892b52688272dec1555"
+                    alt="Codacy Coverage"
+                >
             </a>
             <a href="https://codeclimate.com/github/usetall/tallui/maintainability">
-                <img src="https://api.codeclimate.com/v1/badges/1b6dae4442e751fd60b9/maintainability"
-                    alt="Code Climate Maintainability">
+                <img
+                    src="https://api.codeclimate.com/v1/badges/1b6dae4442e751fd60b9/maintainability"
+                    alt="Code Climate Maintainability"
+                >
             </a>
             <a href="https://app.snyk.io/org/adrolli/project/dd7d7d2c-7a0c-4741-ab01-e3d11ea18fa0">
-                <img alt="Snyk Security" src="https://img.shields.io/snyk/vulnerabilities/github/usetall/tallui">
+                <img
+                    alt="Snyk Security"
+                    src="https://img.shields.io/snyk/vulnerabilities/github/usetall/tallui"
+                >
             </a>
         </div>
         <div class="flex flex-row items-center justify-center gap-3 mt-3">
 
             <a href="https://renovatebot.com/">
-                <img src="https://img.shields.io/badge/renovate-enabled-brightgreen.svg" alt="Renovate" />
+                <img
+                    src="https://img.shields.io/badge/renovate-enabled-brightgreen.svg"
+                    alt="Renovate"
+                />
             </a>
             <a href="https://hosted.weblate.org/engage/tallui/">
-                <img src="https://hosted.weblate.org/widgets/tallui/-/svg-badge.svg" alt="Translation status" />
+                <img
+                    src="https://hosted.weblate.org/widgets/tallui/-/svg-badge.svg"
+                    alt="Translation status"
+                />
             </a>
             <a href="https://github.com/usetall/tallui-app-components/blob/main/LICENSE.md">
-                <img alt="License"
-                    src="https://img.shields.io/github/license/usetall/tallui-app-components?color=blue&label=license">
+                <img
+                    alt="License"
+                    src="https://img.shields.io/github/license/usetall/tallui-app-components?color=blue&label=license"
+                >
             </a>
             <a href="https://tallui.slack.com/">
-                <img alt="Slack" src="https://img.shields.io/badge/Slack-TallUI-blue?logo=slack">
+                <img
+                    alt="Slack"
+                    src="https://img.shields.io/badge/Slack-TallUI-blue?logo=slack"
+                >
             </a>
         </div>
 
