9f59e92f3649658adf3a457a4b7549a2c4412c6a
Alf Drollinger <alf@drollinger.info>
2023-02-21 22:04:03 +0100

    Fix(Monorepo): Recreate all Migrations

--- tests/Feature/PasswordConfirmationTest.php
+++ tests/Feature/PasswordConfirmationTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Laravel\Jetstream\Features;
 use Tests\TestCase;
 
 class PasswordConfirmationTest extends TestCase
