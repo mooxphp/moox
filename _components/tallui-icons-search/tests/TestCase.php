240b1ca2041899479f2a39e26b27434cdbcbbc28
KimSpeer <kim.speer@co-IT.eu>
2023-03-03 11:17:05 +0100

    wip(monorepo):icons-search cleaning

--- _components/tallui-icons-search/tests/TestCase.php
+++ _components/tallui-icons-search/tests/TestCase.php
@@ -20,14 +20,14 @@ class TestCase extends Orchestra
 
         config()->set('app.key', '6rE9Nz59bGRbeMATftriyQjrpF7DcOQm');
 
-        View::addNamespace('test', __DIR__.'/resources/views');
+        View::addNamespace('test', __DIR__ . '/resources/views');
 
         $this
             ->registerLivewireComponents();
         //->registerLivewireTestMacros();
 
         Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'Adrolli\\TestRepoZwo\\Database\\Factories\\'.class_basename($modelName).'Factory'
+            fn (string $modelName) => 'Adrolli\\TestRepoZwo\\Database\\Factories\\' . class_basename($modelName) . 'Factory'
         );
     }
 
@@ -52,7 +52,6 @@ class TestCase extends Orchestra
     private function registerLivewireComponents(): self
     {
         $prefix = config('tallui-icons-search.prefix', '');
-        $assets = config('tallui-icons-search.assets', []);
 
         foreach (config('tallui-icons-search.livewire', []) as $alias => $component) {
             $alias = $prefix ? "$prefix-$alias" : $alias;
