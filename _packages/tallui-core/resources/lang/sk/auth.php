747bbb264329abb489b940ea0bd712184357066d
Milan Šalka <salka.milan@googlemail.com>
2023-08-09 19:01:14 +0000

    Translated using Weblate (Slovak)
    Currently translated at 100.0% (3 of 3 strings)
    
    Translation: TallUI/TallUI Core Auth
    Translate-URL: https://hosted.weblate.org/projects/tallui/tallui-core-auth/sk/

--- _packages/tallui-core/resources/lang/sk/auth.php
+++ _packages/tallui-core/resources/lang/sk/auth.php
@@ -1,9 +1,6 @@
 <?php
-
-declare(strict_types=1);
-
 return [
     'failed' => 'Prihlasovacie údaje nie sú správne.',
-    'password' => 'Heslo nie je správne',
+    'password' => 'Heslo nie je správne.',
     'throttle' => 'Prekročený limit pokusov. Skúste znovu o :seconds sekúnd.',
 ];
