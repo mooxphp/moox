2933e9487ef8b00f5a91b76221c643a9546f3528
Alf Drollinger <40421928+adrolli@users.noreply.github.com>
2023-12-01 00:40:54 +0100

    Remove Team feature

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,6 @@
 
 namespace App\Providers;
 
-use App\Models\Team;
-use App\Policies\TeamPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
