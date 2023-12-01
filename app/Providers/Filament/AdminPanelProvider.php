77e438d6e7083f1553959388878a07cdc2673d5e
KimSpeer <kim.speer@co-IT.eu>
2023-12-01 13:15:19 +0100

    wip package creation

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,6 +28,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->plugin(BuilderPlugin::make())
             ->colors([
                 'primary' => Color::Amber,
             ])
---
+++ packages/builder/src/BuilderServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Moox\Builder;
+
+use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
+
+class BuilderServiceProvider extends PackageServiceProvider
+{
+    public function configurePackage(Package $package): void
+    {
+        $package
+            ->name('builder')
+            ->hasConfigFile()
+            ->hasViews()
+            ->hasTranslations()
+            ->hasMigration('create_builder_table');
+    }
+
+    public function boot()
+    {
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+    }
+}
