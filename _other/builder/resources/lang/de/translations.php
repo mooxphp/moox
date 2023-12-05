0b7bfe9cbd0431b2f72893e99532bb15f6352c50
Alf Drollinger <alf@drollinger.info>
2023-12-05 14:33:20 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (10 of 10 strings)
    
    Translation: Moox/Builder
    Translate-URL: https://hosted.weblate.org/projects/moox/builder/de/

--- _other/builder/resources/lang/de/translations.php
+++ _other/builder/resources/lang/de/translations.php
@@ -1,13 +1,13 @@
 <?php
 return [
-    'breadcrumb' => '',
-    'title' => '',
-    'navigation_label' => '',
-    'navigation_group' => '',
-    'totalone' => '',
-    'totaltwo' => '',
-    'totalthree' => '',
-    'name' => '',
-    'started_at' => '',
-    'failed' => '',
+    'breadcrumb' => 'Builder',
+    'title' => 'Builder',
+    'navigation_label' => 'Builder',
+    'navigation_group' => 'Builder Gruppe',
+    'totalone' => 'Builder Eins',
+    'totaltwo' => 'Builder Zwei',
+    'totalthree' => 'Builder Drei',
+    'name' => 'Name',
+    'started_at' => 'Gestartet',
+    'failed' => 'failed',
 ];
