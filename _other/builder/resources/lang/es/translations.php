617cf2df7bcf950d2b270da56e7782d851fc747d
Alf Drollinger <alf@drollinger.info>
2023-12-06 10:28:42 +0100

    Added translation using Weblate (Spanish)

---
+++ _other/builder/resources/lang/es/translations.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    'breadcrumb' => '',
+    'title' => '',
+    'navigation_label' => '',
+    'navigation_group' => '',
+    'totalone' => '',
+    'totaltwo' => '',
+    'totalthree' => '',
+    'name' => '',
+    'started_at' => '',
+    'failed' => '',
+];
