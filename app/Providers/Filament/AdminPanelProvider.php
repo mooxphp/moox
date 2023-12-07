30a98e1a36c66194357974b7b952e1512ae22121
KimSpeer <kim.speer@co-IT.eu>
2023-12-07 09:07:10 +0100

    Update AdminPanelProvider.php

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,7 +28,6 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
-            ->plugin(BuilderPlugin::make())
             ->colors([
                 'primary' => Color::Amber,
             ])
