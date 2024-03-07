6bb80798806c937f37e85bea44767b05873b34f9
KimSpeer <93331309+Kim-the-Diamond@users.noreply.github.com>
2024-03-07 14:10:01 +0100

    Update FailedJob.php

--- packages/jobs/src/Models/FailedJob.php
+++ packages/jobs/src/Models/FailedJob.php
@@ -7,4 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class FailedJob extends Model
 {
     public $timestamps = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'uuid',
+        'connection',
+        'queue',
+        'failed_at'
+    ];
 }
