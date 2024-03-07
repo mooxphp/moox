32385ca61c265befa8b9a3eb09f435942db4f4b7
Alf Drollinger <alf@drollinger.info>
2024-03-07 15:12:07 +0100

    Jobs V3 FailedJobs phpstan issue

--- packages/jobs/src/Models/FailedJob.php
+++ packages/jobs/src/Models/FailedJob.php
@@ -4,6 +4,13 @@ namespace Moox\Jobs\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $id
+ * @property string $uuid
+ * @property string $connection
+ * @property string $queue
+ * @property \Carbon\Carbon $failed_at
+ */
 class FailedJob extends Model
 {
     public $timestamps = false;
