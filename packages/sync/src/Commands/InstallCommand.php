239645188984921ac85d00f6480169cce1f96324
KimSpeer <93331309+Kim-the-Diamond@users.noreply.github.com>
2024-03-08 12:56:26 +0100

    blank spaces fix

--- packages/sync/src/Commands/InstallCommand.php
+++ packages/sync/src/Commands/InstallCommand.php
@@ -75,11 +75,9 @@ class InstallCommand extends Command
             if (! File::exists('config/sync.php')) {
                 info('Publishing Sync Configuration...');
                 $this->callSilent('vendor:publish', ['--tag' => 'sync-config']);
-
                 return;
             }
             warning('The Sync config already exist. The config will not be published.');
-
         }
     }
 
