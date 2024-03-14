fecd685e309c8b749938451307808e22727d0e74
Alf Drollinger <alf@drollinger.info>
2024-03-14 11:57:34 +0100

    Minor fix

--- packages/user/src/Resources/UserResource/Pages/EditUser.php
+++ packages/user/src/Resources/UserResource/Pages/EditUser.php
@@ -5,6 +5,7 @@ namespace Moox\User\Resources\UserResource\Pages;
 use Filament\Actions\DeleteAction;
 use Filament\Resources\Pages\EditRecord;
 use Illuminate\Support\Facades\Hash;
+use Moox\User\Models\User;
 use Moox\User\Resources\UserResource;
 use STS\FilamentImpersonate\Pages\Actions\Impersonate;
 
@@ -21,7 +22,9 @@ class EditUser extends EditRecord
     {
         if (filled($data['new_password'])) {
             $data = collect($this->form->getState())->only('new_password')->all();
-            $this->record->password = Hash::make($data['new_password']);
+            if ($this->record instanceof User) {
+                $this->record->password = Hash::make($data['new_password']);
+            }
         }
 
         return $data;
