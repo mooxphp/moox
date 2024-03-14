b28fe03e2e716af576d1081f4cb89494617cb2a2
Weblate (bot) <hosted@weblate.org>
2024-03-14 21:30:13 +0100

    Translations update from Hosted Weblate (#417)
    Co-authored-by: gallegonovato <fran-carro@hotmail.es>

--- packages/jobs/resources/lang/es/translations.php
+++ packages/jobs/resources/lang/es/translations.php
@@ -1,5 +1,4 @@
 <?php
-
 return [
     'jobs' => [
         'single' => 'Trabajo En Cola',
@@ -21,11 +20,9 @@ return [
         'plural' => 'Lotes de trabajos',
         'navigation_label' => 'Lotes de trabajos',
     ],
-
     //general
     'breadcrumb' => 'Monitor de Trabajos En Cola',
     'navigation_group' => 'Sistema',
-
     //used by multiple plugins
     'queue' => 'Cola',
     'name' => 'Nombre',
@@ -33,7 +30,6 @@ return [
     'failed' => 'Fallido',
     'waiting' => 'En espera',
     'created_at' => 'Creado a las',
-
     'total_jobs' => 'Total Trabajos Ejecutados',
     'execution_time' => 'Tiempo Total de Ejecución',
     'average_time' => 'Tiempo Promedio de Ejecución',
@@ -44,5 +40,29 @@ return [
     'reserved_at' => 'Reservado a las',
     'waiting_jobs' => 'Número de trabajos en espera',
     'attempts' => 'Intentos',
-
+    'failed_at' => 'Falló en',
+    'delete' => 'Borrar',
+    'finished_at' => 'Completado en',
+    'exception_message' => 'Mensaje de excepción',
+    //jobfailed
+    'uuid' => 'Identificador único universal (Uuid)',
+    'payload' => 'Cola',
+    'connection' => 'Conexión',
+    'exception' => 'Excepción',
+    'retry' => 'Reintentar',
+    'delete_all_failed_jobs' => 'Se eliminaron todos los trabajos fallidos',
+    'delete_all_failed_jobs_notification' => 'Se han eliminado todos los trabajos fallidos.',
+    'jobs_pushed_back_notification' => 'Los trabajos han sido devueltos a la cola.',
+    //jobbatches
+    'canceled_at' => 'Cancelado en',
+    'failed_jobs' => 'Trabajos fallidos',
+    'prune_batches_notification' => 'Todos los lotes han sido purgados.',
+    //used by multiple plugins
+    'id' => 'ID',
+    'failed_job_id' => 'ID del trabajo fallido',
+    'pending_jobs' => 'Trabajos pendientes',
+    'prune_batches' => 'Purgar todos los lotes',
+    'retry_all_failed_jobs' => 'Reintentar todos los trabajos fallidos',
+    'retry_all_failed_jobs_notification' => 'Todos los trabajos fallidos se han puesto a la cola.',
+    'job_pushed_back_notification' => 'está de vuelta en la cola.',
 ];
