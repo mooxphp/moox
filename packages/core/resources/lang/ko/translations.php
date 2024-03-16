22ab0a986083de20d1e001ce687cd68336c08c34
simmon <simmon@nplob.com>
2024-03-16 18:18:30 +0000

    Translated using Weblate (Korean)
    Currently translated at 100.0% (4 of 4 strings)
    
    Translation: Moox/Core
    Translate-URL: https://hosted.weblate.org/projects/moox/core/ko/

--- packages/core/resources/lang/ko/translations.php
+++ packages/core/resources/lang/ko/translations.php
@@ -1,8 +1,7 @@
 <?php
-
 return [
-    'breadcrumb' => '',
-    'title' => '',
-    'navigation_label' => '',
-    'navigation_group' => '',
+    'breadcrumb' => 'Core',
+    'title' => 'Core',
+    'navigation_label' => 'Core',
+    'navigation_group' => 'Moox Core',
 ];
