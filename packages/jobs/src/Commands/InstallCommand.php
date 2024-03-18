4c21c337a29c5573e26b1d067ea385286f7cb754
Alf Drollinger <alf@drollinger.info>
2024-03-18 10:09:01 +0100

    InstallCommand w-i-p

--- packages/jobs/src/Commands/InstallCommand.php
+++ packages/jobs/src/Commands/InstallCommand.php
@@ -69,13 +69,13 @@ class InstallCommand extends Command
     public function checkForFilament(): void
     {
         if (! File::exists($this->providerPath)) {
-            error('The Filament AdminPanelProvider.php or FilamentServiceProvider.php file does not exist.');
+            error('The Filament AdminPanelProvider.php file does not exist.');
             info(' ');
             warning('You should install FilamentPHP first, see https://filamentphp.com/docs/panels/installation');
             info(' ');
             if (confirm('Do you want to install Filament now?', true)) {
                 info('Starting Filament installer...');
-                $this->callSilent('filament:install', ['--panels' => true]);
+                $this->call('filament:install', ['--panels' => true]);
             }
         }
 
