8ed830069f78ec6bcc20895d245528e35b2bd162
Milo Ivir <mail@milotype.de>
2024-03-20 20:48:19 +0100

    Added translation using Weblate (Croatian)

---
+++ packages/sync/resources/lang/hr/translations.php
@@ -0,0 +1,17 @@
+<?php
+return [
+    'single' => '',
+    'plural' => '',
+    'platform' => '',
+    'platforms' => '',
+    'breadcrumb' => '',
+    'title' => '',
+    'navigation_label' => '',
+    'navigation_group' => '',
+    'totalone' => '',
+    'totaltwo' => '',
+    'totalthree' => '',
+    'name' => '',
+    'started_at' => '',
+    'failed' => '',
+];
