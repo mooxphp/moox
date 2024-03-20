b71ea447e4729345d23a063d3c002ad48be13bf7
Milo Ivir <mail@milotype.de>
2024-03-20 20:54:13 +0100

    Added translation using Weblate (Croatian)

---
+++ packages/jobs/resources/lang/hr/translations.php
@@ -0,0 +1,69 @@
+<?php
+return [
+    'jobs' => [
+        'single' => '',
+        'plural' => '',
+        'navigation_label' => '',
+    ],
+    'jobs_waiting' => [
+        'single' => '',
+        'plural' => '',
+        'navigation_label' => '',
+    ],
+    'jobs_failed' => [
+        'single' => '',
+        'plural' => '',
+        'navigation_label' => '',
+    ],
+    'jobs_batches' => [
+        'single' => '',
+        'plural' => '',
+        'navigation_label' => '',
+    ],
+    //general
+    'breadcrumb' => '',
+    'navigation_group' => '',
+    //used by multiple plugins
+    'id' => '',
+    'failed_at' => '',
+    'delete' => '',
+    'queue' => '',
+    'name' => '',
+    'started_at' => '',
+    'finished_at' => '',
+    'failed' => '',
+    'waiting' => '',
+    'exception_message' => '',
+    'created_at' => '',
+    //jobs
+    'status' => '',
+    'running' => '',
+    'succeeded' => '',
+    'progress' => '',
+    //Jobswaiting
+    'attempts' => '',
+    'reserved_at' => '',
+    'waiting_jobs' => '',
+    'execution_time' => '',
+    'average_time' => '',
+    //jobfailed
+    'uuid' => '',
+    'payload' => '',
+    'connection' => '',
+    'exception' => '',
+    'retry' => '',
+    'retry_all_failed_jobs' => '',
+    'retry_all_failed_jobs_notification' => '',
+    'delete_all_failed_jobs' => '',
+    'delete_all_failed_jobs_notification' => '',
+    'jobs_pushed_back_notification' => '',
+    'job_pushed_back_notification' => '',
+    //jobbatches
+    'canceled_at' => '',
+    'failed_jobs' => '',
+    'failed_job_id' => '',
+    'pending_jobs' => '',
+    'total_jobs' => '',
+    'prune_batches' => '',
+    'prune_batches_notification' => '',
+];
