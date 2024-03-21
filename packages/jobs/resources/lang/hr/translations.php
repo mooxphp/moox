646044d801b44556b30cb3ca2ee5c95951ac6fad
Weblate (bot) <noreply@weblate.org>
2024-03-21 09:11:42 +0100

    Translations update from Hosted Weblate (#445)
    Co-authored-by: Milo Ivir <mail@milotype.de>

---
+++ packages/jobs/resources/lang/hr/translations.php
@@ -0,0 +1,69 @@
+<?php
+return [
+    'jobs' => [
+        'single' => '',
+        'plural' => '',
+        'navigation_label' => '',
+    ],
+    'jobs_waiting' => [
+        'single' => '',
+        'plural' => '',
+        'navigation_label' => '',
+    ],
+    'jobs_failed' => [
+        'single' => '',
+        'plural' => '',
+        'navigation_label' => '',
+    ],
+    'jobs_batches' => [
+        'single' => '',
+        'plural' => '',
+        'navigation_label' => '',
+    ],
+    //general
+    'breadcrumb' => '',
+    'navigation_group' => '',
+    //used by multiple plugins
+    'id' => 'ID',
+    'failed_at' => 'Neuspjelo',
+    'delete' => 'Izbriši',
+    'queue' => '',
+    'name' => 'Ime',
+    'started_at' => 'Pokrenuto',
+    'finished_at' => 'Završeno',
+    'failed' => 'Neuspjelo',
+    'waiting' => 'Čeka',
+    'exception_message' => 'Poruka za iznimku',
+    'created_at' => 'Stvoreno',
+    //jobs
+    'status' => 'Stanje',
+    'running' => '',
+    'succeeded' => 'Uspjelo',
+    'progress' => 'Napredak',
+    //Jobswaiting
+    'attempts' => 'Pokušaji',
+    'reserved_at' => 'Rezervirano',
+    'waiting_jobs' => '',
+    'execution_time' => 'Ukupno vrijeme izvršavanja',
+    'average_time' => 'Prosječno vrijeme izvršavanja',
+    //jobfailed
+    'uuid' => 'Univerzalni jedinstveni identifikator (Uuid)',
+    'payload' => '',
+    'connection' => 'Veza',
+    'exception' => 'Iznimka',
+    'retry' => 'Ponovi',
+    'retry_all_failed_jobs' => 'Ponovi sve neuspjele poslove',
+    'retry_all_failed_jobs_notification' => '',
+    'delete_all_failed_jobs' => 'Izbrisani su svi neuspjeli poslovi',
+    'delete_all_failed_jobs_notification' => 'Svi neuspjeli poslovi su uklonjeni.',
+    'jobs_pushed_back_notification' => '',
+    'job_pushed_back_notification' => '',
+    //jobbatches
+    'canceled_at' => 'Prekinuto',
+    'failed_jobs' => 'Neuspjeli poslovi',
+    'failed_job_id' => 'ID neuspjelog posla',
+    'pending_jobs' => '',
+    'total_jobs' => 'Ukupno izvršenih poslova',
+    'prune_batches' => '',
+    'prune_batches_notification' => '',
+];
