f0187423c1ce95a3c05daa2eec734b3f2b45f4b1
Weblate (bot) <noreply@weblate.org>
2024-03-21 09:05:27 +0100

    Translations update from Hosted Weblate (#441)
    Co-authored-by: Milo Ivir <mail@milotype.de>

---
+++ packages/user/resources/lang/hr/translations.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'single' => 'Korisnik',
+    'plural' => 'Korisnici',
+    'breadcrumb' => 'Korisnik',
+    'title' => 'Korisnik',
+    'navigation_label' => 'Korisnici',
+    'navigation_group' => 'Moox korisnik',
+    'name' => 'Ime',
+];
