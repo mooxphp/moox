b495db2548312db9977b236501d5285ea980c9ea
Weblate (bot) <noreply@weblate.org>
2024-04-01 19:35:02 +0200

    Translations update from Hosted Weblate (#472)
    Co-authored-by: Mikachu <micah.sh@proton.me>

---
+++ packages/user/resources/lang/nl/translations.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'single' => 'Gebruiker',
+    'plural' => 'Gebruikers',
+    'breadcrumb' => 'Gebruiker',
+    'title' => 'Gebruiker',
+    'navigation_label' => 'Gebruikers',
+    'navigation_group' => 'Moox-gebruiker',
+    'name' => 'Naam',
+];
