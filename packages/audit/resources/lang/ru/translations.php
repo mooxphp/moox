6b5586de3ee4c9f0db55f8268b6ad89a5ef6ee0b
Weblate (bot) <noreply@weblate.org>
2024-04-08 21:59:15 +0200

    Translations update from Hosted Weblate (#482)
    Co-authored-by: Mika <akimivanov43@gmail.com>
    Co-authored-by: Alf Drollinger <40421928+adrolli@users.noreply.github.com>

---
+++ packages/audit/resources/lang/ru/translations.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'single' => 'Аудит',
+    'plural' => 'Аудиты',
+    'breadcrumb' => 'Аудит',
+    'title' => 'Аудит',
+    'navigation_label' => 'Аудит',
+    'navigation_group' => 'Moox Аудит',
+    'totalone' => 'Первый аудит',
+    'totaltwo' => 'Второй аудит',
+    'totalthree' => 'Третий аудит',
+    'name' => 'Имя',
+    'started_at' => 'Начато в',
+    'failed' => 'Неудача',
+];
