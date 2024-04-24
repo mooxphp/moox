e39872e13a5b8d7b4b50ad7aa7c290c18823522f
Alf Drollinger <alf@drollinger.info>
2024-04-25 01:05:48 +0200

    Init Login-Link

---
+++ packages/login-link/src/Commands/InstallCommand.php
@@ -0,0 +1,163 @@
+<?php
+
+namespace Moox\LoginLink\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Schema;
+
+use function Laravel\Prompts\alert;
+use function Laravel\Prompts\confirm;
+use function Laravel\Prompts\info;
+use function Laravel\Prompts\multiselect;
+use function Laravel\Prompts\note;
+use function Laravel\Prompts\warning;
+
+class InstallCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'mooxlogin-link:install';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Installs Moox LoginLink, publishes configuration, migrations and registers plugins.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $this->art();
+        $this->welcome();
+        $this->publishConfiguration();
+        $this->publishMigrations();
+        $this->runMigrations();
+        $this->registerPlugins();
+        $this->finish();
+    }
+
+    public function art(): void
+    {
+        info('
+
+        ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▓▓▓▓▓▓       ▓▓▓▓▓▓▓▓▓▓▓▓           ▓▓▓▓▓▓▓▓▓▓▓▓   ▓▓▓▓▓▓▓        ▓▓▓▓▓▓▓
+        ▓▓▒░░▒▓▓▒▒░░░░░░▒▒▓▓▓▒░░░░░░░▒▓▓   ▓▓▓▓▒░░░░░░░▒▓▓▓▓     ▓▓▓▓▓▒░░░░░░░▒▒▓▓▓▓▓▒▒▒▒▓▓      ▓▓▓▒▒▒▒▓▓
+        ▓▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▓▓▓▓▓▒░░░░░░░░░░░░░▒▓▓▓ ▓▓▓▓▒░░░░░░░░░░░░░▒▓▓▓░░░░░▒▓▓   ▓▓▒░░░░░▓▓
+        ▓▒░░░░░░▒▓▓▓▓▒░░░░░░░▒▓▓▓▓░░░░░▒▓▓▓░░░░░▒▓▓▓▓▒░░░░░░░▓▓▓▓░░░░░░▒▓▓▓▓▓░░░░░░▒▓▓░░░░░▒▓▓▓▓▓░░░░░▒▓▓
+        ▓▒░░░░▓▓▓▓  ▓▓░░░░░▓▓▓  ▓▓▓░░░░▒▓▓░░░░▒▓▓▓   ▓▓▓▓░░░░░▓░░░░░░▓▓▓▓   ▓▓▓▒░░░░▓▓▓▒░░░░░▓▓▓░░░░░▓▓▓
+        ▓▒░░░░▒▓    ▓▓░░░░░▓▓    ▓▓░░░░▒▓░░░░▒▓▓        ▓▓▓░░▒░░░░░▓▓▓        ▓▓░░░░▒▓▓▓▓░░░░░░░░░░░▓▓
+        ▓▒░░░░▒▓    ▓▓░░░░░▓▓    ▓▓░░░░▒▓░░░░▒▓          ▓▓▓░░░░░▒▓▓          ▓▓▒░░░░▓ ▓▓▓░░░░░░░░░▓▓
+        ▓▒░░░░▒▓    ▓▓░░░░░▓▓    ▓▓░░░░▒▓░░░░▒▓▓        ▓▓▒░░░░░▒░░▒▓▓        ▓▓░░░░▒▓▓▓▒░░░░░▒░░░░░▒▓
+        ▓▒░░░░▒▓    ▓▓░░░░░▓▓    ▓▓░░░░▒▓▓░░░░▒▓▓▓   ▓▓▓▒░░░░░▒▒░░░░░▒▓▓▓   ▓▓▓░░░░░▓▓▓░░░░░▒▓▓▓░░░░░▒▓▓
+        ▓▒░░░░▒▓    ▓▓░░░░░▓▓    ▓▓░░░░▒▓▓▓░░░░░░▒▒▓▓▒░░░░░░▒▓▓▓▓░░░░░░░▒▒▓▓▒░░░░░░▓▓▓░░░░░▒▓▓▓▓▓▒░░░░░▓▓
+        ▓▒░░░░▒▓    ▓▓░░░░░▓▓    ▓▓░░░░▒▓▓▓▓▒░░░░░░░░░░░░░▒▓▓▓ ▓▓▓▓▒░░░░░░░░░░░░░▒▓▓▒░░░░░▓▓▓   ▓▓▒░░░░░▒▓
+        ▓▓░░░▒▓▓    ▓▓▒░░░▒▓▓    ▓▓░░░░▓▓  ▓▓▓▓▒░░░░░░▒▒▓▓▓▓     ▓▓▓▓▓▒▒░░░░░▒▒▓▓▓▓▓░░░░▒▓▓      ▓▓▓░░░░▒▓
+        ▓▓▓▓▓▓▓      ▓▓▓▓▓▓▓     ▓▓▓▓▓▓▓▓    ▓▓▓▓▓▓▓▓▓▓▓▓           ▓▓▓▓▓▓▓▓▓▓▓▓  ▓▓▓▓▓▓▓▓        ▓▓▓▓▓▓▓▓
+
+        ');
+    }
+
+    public function welcome(): void
+    {
+        info('Welcome to Moox LoginLink Installer');
+    }
+
+    public function publishConfiguration(): void
+    {
+        if (confirm('Do you wish to publish the configuration?', true)) {
+            if (! File::exists('config/login-link.php')) {
+                info('Publishing LoginLink Configuration...');
+                $this->callSilent('vendor:publish', ['--tag' => 'login-link-config']);
+
+                return;
+            }
+            warning('The LoginLink config already exist. The config will not be published.');
+        }
+    }
+
+    public function publishMigrations(): void
+    {
+        if (confirm('Do you wish to publish the migrations?', true)) {
+            if (Schema::hasTable('login_links')) {
+                warning('The login_links table already exists. The migrations will not be published.');
+
+                return;
+            }
+            info('Publishing LoginLinks Migrations...');
+            $this->callSilent('vendor:publish', ['--tag' => 'login-link-migrations']);
+        }
+    }
+
+    public function runMigrations(): void
+    {
+        if (confirm('Do you wish to run the migrations?', true)) {
+            info('Running LoginLink Migrations...');
+            $this->callSilent('migrate');
+        }
+    }
+
+    public function registerPlugins(): void
+    {
+        $providerPath = app_path('Providers/Filament/AdminPanelProvider.php');
+
+        if (File::exists($providerPath)) {
+            $content = File::get($providerPath);
+
+            $intend = '                ';
+
+            $namespace = "\Moox\LoginLink";
+
+            $pluginsToAdd = multiselect(
+                label: 'These plugins will be installed:',
+                options: ['LoginLinkPlugin'],
+                default: ['LoginLinkPlugin'],
+            );
+
+            $function = '::make(),';
+
+            $pattern = '/->plugins\(\[([\s\S]*?)\]\);/';
+            $newPlugins = '';
+
+            foreach ($pluginsToAdd as $plugin) {
+                $searchPlugin = '/'.$plugin.'/';
+                if (preg_match($searchPlugin, $content)) {
+                    warning("$plugin already registered.");
+                } else {
+                    $newPlugins .= $intend.$namespace.'\\'.$plugin.$function."\n";
+                }
+            }
+
+            if ($newPlugins) {
+                if (preg_match($pattern, $content)) {
+                    info('Plugins section found. Adding new plugins...');
+
+                    $replacement = "->plugins([$1\n$newPlugins\n            ]);";
+                    $newContent = preg_replace($pattern, $replacement, $content);
+                } else {
+                    info('Plugins section created. Adding new plugins...');
+
+                    $pluginsSection = "            ->plugins([\n$newPlugins\n            ]);";
+                    $placeholderPattern = '/(\->authMiddleware\(\[.*?\]\))\s*\;/s';
+                    $replacement = "$1\n".$pluginsSection;
+                    $newContent = preg_replace($placeholderPattern, $replacement, $content, 1);
+                }
+
+                File::put($providerPath, $newContent);
+            }
+        } else {
+            alert('AdminPanelProvider not found. You need to add the plugins manually.');
+        }
+    }
+
+    public function finish(): void
+    {
+        note('Moox LoginLink installed successfully. Enjoy!');
+    }
+}
