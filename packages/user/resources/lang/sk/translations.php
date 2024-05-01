f3134250f62f803d6643d675fe76edd6de2edb51
Weblate (bot) <noreply@weblate.org>
2024-05-02 01:16:01 +0200

    Translations update from Hosted Weblate (#519)
    Co-authored-by: Milan Šalka <salka.milan@googlemail.com>
    Co-authored-by: Alf Drollinger <40421928+adrolli@users.noreply.github.com>

---
+++ packages/user/resources/lang/sk/translations.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'single' => 'Uživateľ',
+    'plural' => 'Uživatelia',
+    'breadcrumb' => 'Uživateľ',
+    'title' => 'Uživateľ',
+    'navigation_label' => 'Uživatelia',
+    'navigation_group' => 'Moox Užívateľ',
+    'name' => 'Názov',
+];
