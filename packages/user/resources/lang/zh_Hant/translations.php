ab074233d6be87eac10f37ec162bed82cc05c23c
hugoalh <hugoalh@users.noreply.hosted.weblate.org>
2024-06-10 04:33:02 +0000

    Translated using Weblate (Chinese (Traditional))
    Currently translated at 100.0% (7 of 7 strings)
    
    Translation: Moox/User
    Translate-URL: https://hosted.weblate.org/projects/moox/user/zh_Hant/

--- packages/user/resources/lang/zh_Hant/translations.php
+++ packages/user/resources/lang/zh_Hant/translations.php
@@ -1,10 +1,10 @@
 <?php
 return [
-    'single' => '',
-    'plural' => '',
-    'breadcrumb' => '',
-    'title' => '',
-    'navigation_label' => '',
-    'navigation_group' => '',
-    'name' => '',
+    'single' => '使用者',
+    'plural' => '使用者',
+    'breadcrumb' => '使用者',
+    'title' => '使用者',
+    'navigation_label' => '使用者',
+    'navigation_group' => 'Moox 使用者',
+    'name' => '名稱',
 ];
