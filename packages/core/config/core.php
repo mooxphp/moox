90ea72f05adaa118c1c3fb244532d35a5c2f425a
Alf Drollinger <alf@drollinger.info>
2024-06-20 13:44:47 +0200

    Update core.php

--- packages/core/config/core.php
+++ packages/core/config/core.php
@@ -8,6 +8,6 @@ return [
      | automatically loaded, if it is installed and enabled
      | https://filamentphp.com/plugins/kenneth-sese-advanced-tables
     */
-    'use_advanced_tables' => false,
+    'use_advanced_tables' => true,
 
 ];
