984462e3652f8df25eb40c04a4f5b31a39c7bd30
Alf Drollinger <alf@drollinger.info>
2024-06-20 16:01:13 +0200

    Update BaseUser.php

--- packages/core/src/Base/BaseUser.php
+++ packages/core/src/Base/BaseUser.php
@@ -3,17 +3,28 @@
 namespace Moox\Core\Base;
 
 use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 
 if (config('core.use_advanced_tables') === true && trait_exists('\Archilex\AdvancedTables\AdvancedTables')) {
-    class BaseUser extends FilamentUser
+    class BaseUser implements FilamentUser
     {
         use \Archilex\AdvancedTables\Concerns\HasViews;
 
         public bool $useAdvancedViews = true;
+
+        public function canAccessPanel(Panel $panel): bool
+        {
+            return true;
+        }
     }
 } else {
-    class BaseUser extends FilamentUser
+    class BaseUser implements FilamentUser
     {
         public bool $useAdvancedViews = false;
+
+        public function canAccessPanel(Panel $panel): bool
+        {
+            return true;
+        }
     }
 }
