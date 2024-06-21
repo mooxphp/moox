e8742a716389410aea6b8a03dabd6e52a6d505d8
Alf Drollinger <alf@drollinger.info>
2024-06-21 12:10:54 +0200

    BaseUser wip

--- packages/core/src/Base/BaseUser.php
+++ packages/core/src/Base/BaseUser.php
@@ -5,7 +5,7 @@ namespace Moox\Core\Base;
 use Illuminate\Contracts\Auth\Authenticatable;
 
 if (config('core.use_advanced_tables') === true && trait_exists('\Archilex\AdvancedTables\AdvancedTables')) {
-    class BaseUser extends Authenticatable
+    abstract class BaseUser extends Authenticatable
     {
         use \Archilex\AdvancedTables\Concerns\HasViews;
 
