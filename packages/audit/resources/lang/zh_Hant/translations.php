b6c48f71e4d2b54582f8b4c987586688980a4095
Weblate (bot) <noreply@weblate.org>
2024-06-24 11:24:02 +0200

    Translations update from Hosted Weblate (#545)
    Co-authored-by: Mika <akimivanov43@gmail.com>
    Co-authored-by: Alf Drollinger <40421928+adrolli@users.noreply.github.com>
    Co-authored-by: hugoalh <hugoalh@users.noreply.hosted.weblate.org>

---
+++ packages/audit/resources/lang/zh_Hant/translations.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'single' => '審計',
+    'plural' => '審計',
+    'breadcrumb' => '審計',
+    'title' => '審計',
+    'navigation_label' => '審計',
+    'navigation_group' => 'Moox 審計',
+    'totalone' => '審計 1',
+    'totaltwo' => '審計 2',
+    'totalthree' => '審計 3',
+    'name' => '名稱',
+    'started_at' => '已開始於',
+    'failed' => '已失敗',
+];
