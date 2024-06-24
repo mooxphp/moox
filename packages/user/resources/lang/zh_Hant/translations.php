5e5f5072a895d283c87861d0713bc78c55764740
Weblate (bot) <noreply@weblate.org>
2024-06-24 11:22:12 +0200

    Translations update from Hosted Weblate (#543)
    Co-authored-by: Mika <akimivanov43@gmail.com>
    Co-authored-by: Alf Drollinger <40421928+adrolli@users.noreply.github.com>
    Co-authored-by: hugoalh <hugoalh@users.noreply.hosted.weblate.org>

---
+++ packages/user/resources/lang/zh_Hant/translations.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'single' => '使用者',
+    'plural' => '使用者',
+    'breadcrumb' => '使用者',
+    'title' => '使用者',
+    'navigation_label' => '使用者',
+    'navigation_group' => 'Moox 使用者',
+    'name' => '名稱',
+];
