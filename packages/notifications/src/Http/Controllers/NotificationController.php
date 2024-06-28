24b554413327db89213677cb0d532d12c3447742
KimSpeer <kim.speer@web.de>
2024-06-28 17:01:17 +0200

    added api

---
+++ packages/notifications/src/Http/Controllers/NotificationController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Moox\Notification\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class NotificationController extends Controller
+{
+    public function index(){
+        return 'api';
+    }
+}
--- packages/notifications/src/NotificationServiceProvider.php
+++ packages/notifications/src/NotificationServiceProvider.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace Moox\Notification;
 
-use Moox\Notification\Commands\InstallCommand;
+use Illuminate\Support\Facades\Route;
 use Spatie\LaravelPackageTools\Package;
+use Moox\Notification\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
+use Moox\Notification\Http\Controllers\NotificationController;
 
 class NotificationServiceProvider extends PackageServiceProvider
 {
@@ -19,5 +21,10 @@ class NotificationServiceProvider extends PackageServiceProvider
             ->hasTranslations()
             ->hasMigrations(['create_notifications_table'])
             ->hasCommand(InstallCommand::class);
+    }
+
+    public function packageRegistered()
+    {
+        Route::get('notification',[NotificationController::class,'index']);
     }
 }
