1ae63303e5ffd4e38b60a8f54cf7a11f3774936c
Kim-the-Diamond <Kim-the-Diamond@users.noreply.github.com>
2024-07-23 14:19:06 +0000

    Fix styling

--- app/Providers/Filament/PressPanelProvider.php
+++ app/Providers/Filament/PressPanelProvider.php
@@ -2,25 +2,24 @@
 
 namespace App\Providers\Filament;
 
-use Filament\Pages;
-use Filament\Panel;
-use Filament\Widgets;
-use Filament\PanelProvider;
-
-use Moox\Security\Services\Login;
-use Filament\Support\Colors\Color;
-use Moox\Security\Services\ResetPassword;
 use Filament\Http\Middleware\Authenticate;
-use Illuminate\Session\Middleware\StartSession;
-use Illuminate\Cookie\Middleware\EncryptCookies;
-use Moox\Security\Services\RequestPasswordReset;
-use Illuminate\Routing\Middleware\SubstituteBindings;
-use Illuminate\Session\Middleware\AuthenticateSession;
-use Illuminate\View\Middleware\ShareErrorsFromSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
-use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+use Filament\Pages;
+use Filament\Panel;
+use Filament\PanelProvider;
+use Filament\Support\Colors\Color;
+use Filament\Widgets;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+use Illuminate\Cookie\Middleware\EncryptCookies;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+use Illuminate\Routing\Middleware\SubstituteBindings;
+use Illuminate\Session\Middleware\AuthenticateSession;
+use Illuminate\Session\Middleware\StartSession;
+use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Moox\Security\Services\Login;
+use Moox\Security\Services\RequestPasswordReset;
+use Moox\Security\Services\ResetPassword;
 
 class PressPanelProvider extends PanelProvider
 {
