bd18ba4e8adedab9f3c65cc719b190fd561d134f
KimSpeer <kim.speer@web.de>
2024-07-23 15:42:20 +0200

    wip security

--- config/app.php
+++ config/app.php
@@ -194,6 +194,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\Filament\AdminPanelProvider::class,
+        App\Providers\Filament\PressPanelProvider::class,
         App\Providers\RouteServiceProvider::class,
     ],
 
