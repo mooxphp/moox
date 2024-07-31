409ed2b8b593f535e27dec40004cc7f5c723d040
Alf Drollinger <alf@drollinger.info>
2024-07-31 15:24:12 +0200

    Blueprint wip

--- packages/core/config/core.php
+++ packages/core/config/core.php
@@ -10,4 +10,50 @@ return [
     */
     'use_advanced_tables' => true,
 
+    /*
+     | Blueprint of all Moox-compatible models,
+     | if they are authenticatable models (like Users)
+     | and what APIs Services and Routes they possibly provide.
+    */
+    'models' => [
+        'User' => [
+            'Authenticatable' => true,
+            'API' => [
+                'Index' => '',
+                'Show' => '',
+                'Update' => '',
+                'Delete' => '',
+            ],
+            'AuthRoutes' => [
+                'Login' => '',
+                'PasswordReset' => '',
+                'Register' => '',
+            ],
+        ],
+        'WpPost' => [
+            'Authenticatable' => false,
+            'API' => [
+                'Index' => '',
+                'Show' => '',
+                'Update' => '',
+                'Delete' => '',
+            ],
+        ],
+        'WpUser' => [
+            'Authenticatable' => true,
+            'API' => [
+                'Index' => '',
+                'Show' => '',
+                'Update' => '',
+                'Delete' => '',
+            ],
+            'AuthRoutes' => [
+                'Login' => '',
+                'PasswordReset' => '',
+                'Register' => '',
+            ],
+        ],
+
+    ],
+
 ];
