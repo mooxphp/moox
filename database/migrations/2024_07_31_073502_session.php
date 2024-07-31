ce1d313a2f56088098216762642e8cae1d5d38ee
adrolli <adrolli@users.noreply.github.com>
2024-07-31 13:27:47 +0000

    Fix styling

--- database/migrations/2024_07_31_073502_session.php
+++ database/migrations/2024_07_31_073502_session.php
@@ -4,12 +4,11 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     public function up(): void
     {
-        Schema::table('sessions', function (Blueprint $table) {
-
-        });
+        Schema::table('sessions', function (Blueprint $table) {});
     }
 
     public function down(): void
