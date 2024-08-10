fccbab78d7f2944bca2b98d8b4224d4a65d3f8d2
Mikachu <micah.sh@proton.me>
2024-08-10 21:34:24 +0200

    Added translation using Weblate (Dutch)

---
+++ packages/core/resources/lang/nl/post.php
@@ -0,0 +1,28 @@
+<?php
+return [
+    // Press refers to the package name Moox Press, don't translate this
+    'press' => '',
+    // Posts refers to the posts in the package Moox Press (WordPress)
+    'post' => '',
+    // Posts refers to the posts in the package Moox Press (WordPress)
+    'posts' => '',
+    'post_author' => '',
+    'post_date' => '',
+    'post_date_gmt' => '',
+    'post_content' => '',
+    'post_title' => '',
+    'post_excerpt' => '',
+    'post_status' => '',
+    'ping_status' => '',
+    'post_password' => '',
+    'post_name' => '',
+    'post_id' => '',
+    'to_ping' => '',
+    'pinged' => '',
+    'post_modified' => '',
+    'post_modified_gmt' => '',
+    'post_content_filtered' => '',
+    'post_parent' => '',
+    'post_type' => '',
+    'post_mime_type' => '',
+];
