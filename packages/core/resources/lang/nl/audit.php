eae88490a67214ccbb781ac85e84b48ee856cdd8
Mikachu <micah.sh@proton.me>
2024-08-10 13:26:26 +0200

    Added translation using Weblate (Dutch)

---
+++ packages/core/resources/lang/nl/audit.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    // Audit refers to Logging of changes in the system and the package name Moox Audit
+    'audit' => '',
+    // Audits refers to Logging of changes in the system and the package name Moox Audit
+    'audits' => '',
+    // The causer type is the type of the user who caused the log entry
+    'causer_type' => '',
+    // The causer id is the id of the user who caused the log entry
+    'causer_id' => '',
+    // Batch uuid is the unique identifier of the batch of logs
+    'batch_uuid' => '',
+    // Log name refers to the name of the logfile
+    'log_name' => '',
+];
