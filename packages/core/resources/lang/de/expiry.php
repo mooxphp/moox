52298e5c759973d3be00b2e82ac9932d18cbe7e2
Ettore Atalan <atalanttore@googlemail.com>
2024-08-10 18:51:48 +0000

    Translated using Weblate (German)
    Currently translated at 75.0% (3 of 4 strings)
    
    Translation: Moox/Expiry
    Translate-URL: https://hosted.weblate.org/projects/moox/expiry/de/

--- packages/core/resources/lang/de/expiry.php
+++ packages/core/resources/lang/de/expiry.php
@@ -1,17 +1,11 @@
 <?php
-
 return [
-
     // Expiry refers to the expiration date of a record and the package name Moox Expiry
     'expiry' => 'Expiry',
-
     // Expiries refers to the expiration date of a record and the package name Moox Expiry
     'expiries' => 'Expiries',
-
     // No assignee refers to the absence of an assignee for an expired record
-    'no_assignee' => 'No assignee',
-
+    'no_assignee' => '',
     // No expiry date refers to the absence of an expiry date for a record
     'no_expiry_date' => 'No expiry date',
-
 ];
