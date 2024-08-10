4ea6da11c500ca54f73b645101830a12501d97ae
Ettore Atalan <atalanttore@googlemail.com>
2024-08-10 18:58:01 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (22 of 22 strings)
    
    Translation: Moox/Comment
    Translate-URL: https://hosted.weblate.org/projects/moox/comment/de/

--- packages/core/resources/lang/de/comment.php
+++ packages/core/resources/lang/de/comment.php
@@ -1,10 +1,8 @@
 <?php
-
 return [
-
     'comment' => 'Comment',
-    'comments' => 'Comments',
-    'no_comments' => 'No comments',
+    'comments' => 'Kommentare',
+    'no_comments' => 'Keine Kommentare',
     'no_comment' => 'No comment',
     'comment_id' => 'Comment ID',
     'comment_post_ID' => 'Comment Post ID',
@@ -16,7 +14,7 @@ return [
     'comment_date_gmt' => 'Comment Date GMT',
     'comment_content' => 'Comment Content',
     'comment_karma' => 'Comment Karma',
-    'comment_approved' => 'Comment approved',
+    'comment_approved' => 'Kommentar genehmigt',
     'comment_agent' => 'Comment Agent',
     'comment_parent' => 'Comment Parent',
     'comment_type' => 'Comment Type',
@@ -24,5 +22,4 @@ return [
     'comment_count' => 'Comment count',
     'wp_comment_meta' => 'WP Comment Meta',
     'wp_comment_metas' => 'WP Comment Metas',
-
 ];
