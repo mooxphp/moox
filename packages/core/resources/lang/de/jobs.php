dcf897bd24d89dcfc3d327216a3181f9f9b4640f
Weblate (bot) <noreply@weblate.org>
2024-08-12 09:11:02 +0200

    Translations update from Hosted Weblate (#678)
    Co-authored-by: Ettore Atalan <atalanttore@googlemail.com>
    Co-authored-by: Mikachu <micah.sh@proton.me>
    Co-authored-by: Alf Drollinger <40421928+adrolli@users.noreply.github.com>

--- packages/core/resources/lang/de/jobs.php
+++ packages/core/resources/lang/de/jobs.php
@@ -1,74 +1,49 @@
 <?php
-
 return [
-
     // Job refers to Laravel's job queue, should not be translated
-    'job' => 'Job',
-
+    'job' => 'Auftrag',
     // Jobs refers to Laravel's job queue, should not be translated
-    'jobs' => 'Jobs',
-
+    'jobs' => 'Aufträge',
     // Queue refers to Laravel's job queue, should not be translated
-    'queue' => 'Queue',
-
+    'queue' => 'Warteschlange',
     // Queues refers to Laravel's job queue, should not be translated
-    'queues' => 'Queues',
-
+    'queues' => 'Warteschlangen',
     // Job refers to Laravel's job queue, should not be translated
-    'job_waiting' => 'Job Waiting',
-
+    'job_waiting' => 'Auftrag wartet',
     // Jobs refers to Laravel's job queue, should not be translated
-    'jobs_waiting' => 'Jobs Waiting',
-
+    'jobs_waiting' => 'Aufträge warten',
     // Job refers to Laravel's job queue, should not be translated
-    'job_failed' => 'Job Failed',
-
+    'job_failed' => 'Auftrag fehlgeschlagen',
     // Jobs refers to Laravel's job queue, should not be translated
-    'jobs_failed' => 'Jobs Failed',
-
+    'jobs_failed' => 'Aufträge fehlgeschlagen',
     // Jobs Batch refers to Laravel's job queue, should not be translated
     'jobs_batch' => 'Job Batch',
-
     // Jobs Batches refers to Laravel's job queue, should not be translated
     'jobs_batches' => 'Jobs Batches',
-
     // Jobs refers to Laravel's job queue, should not be translated
     'waiting_jobs' => 'Total Jobs Waiting',
-
     // Jobs refers to Laravel's job queue, should not be translated
     'retry_all_failed_jobs' => 'Retry all failed Jobs',
-
     // Jobs refers to Laravel's job queue, should not be translated
     'retry_all_failed_jobs_notification' => 'All failed jobs have been pushed back onto the queue.',
-
     // Jobs refers to Laravel's job queue, should not be translated
-    'delete_all_failed_jobs' => 'Deleted all failed Jobs',
-
+    'delete_all_failed_jobs' => 'Alle fehlgeschlagenen Aufträge löschen',
     // Jobs refers to Laravel's job queue, should not be translated
-    'delete_all_failed_jobs_notification' => 'All failed jobs have been removed.',
-
+    'delete_all_failed_jobs_notification' => 'Alle fehlgeschlagenen Aufträge wurden entfernt.',
     // Jobs refers to Laravel's job queue, should not be translated
     'jobs_pushed_back_notification' => 'jobs have been pushed back onto the queue.',
-
     // Jobs refers to Laravel's job queue, should not be translated
     'job_pushed_back_notification' => 'has been pushed back onto the queue.',
-
     // Jobs refers to Laravel's job queue, should not be translated
-    'failed_jobs' => 'Failed Jobs',
-
+    'failed_jobs' => 'Fehlgeschlagene Aufträge',
     // Jobs refers to Laravel's job queue, should not be translated
     'failed_job_id' => 'Failed Job id',
-
     // Jobs refers to Laravel's job queue, should not be translated
-    'pending_jobs' => 'Pending Jobs',
-
+    'pending_jobs' => 'Ausstehende Aufträge',
     // Jobs refers to Laravel's job queue, should not be translated
     'total_jobs' => 'Total Jobs Executed',
-
     // Batches refers to Laravel's job queue, should not be translated
     'prune_batches' => 'Prune all batches',
-
     // Batches refers to Laravel's job queue, should not be translated
     'prune_batches_notification' => 'All batches have been pruned.',
-
 ];
