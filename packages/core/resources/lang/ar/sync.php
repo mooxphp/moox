d2d935576493fcd62a6c6200d541dfa87e2ff906
Anonymous <noreply@weblate.org>
2024-08-12 07:49:55 +0000

    Translated using Weblate (Arabic)
    Currently translated at 90.6% (29 of 32 strings)
    
    Translation: Moox/Sync
    Translate-URL: https://hosted.weblate.org/projects/moox/sync/ar/

--- packages/core/resources/lang/ar/sync.php
+++ packages/core/resources/lang/ar/sync.php
@@ -1,101 +1,67 @@
 <?php
-
 return [
-
     // Sync refers to the package name Moox Sync, and data transfer between platforms
     'sync' => 'Sync',
-
     // Syncs refers to the package name Moox Sync, and data transfer between platforms
     'syncs' => 'Syncs',
-
     // Platform refers to the platform that data is being transferred from or to
     'platform' => 'Platform',
-
     // Platforms refers to the platforms that data is being transferred from or to
     'platforms' => 'Platforms',
-
     // Source Platform refers to the platform that the data is being transferred from
     'source_platform' => 'Source Platform',
-
     // Target Platform refers to the platform that the data is being transferred to
     'target_platform' => 'Target Platform',
-
     // Target Platforms refers to the platform that the data is being transferred to
     'target_platforms' => 'Target Platforms',
-
     // Source Platform refers to the platform that data is being transferred from
     'source_platform_id' => 'Source Platform ID',
-
     // Target Platform refers to the platform that data is being transferred to
     'target_platform_id' => 'Target Platform ID',
-
     // Sync error refers to any error that occurs in the package Moox Sync
     'sync_error' => 'Sync Error',
-
     // Sync error: if someone tries to make multiple platforms the master platform
     'sync_error_master' => 'There can only be one master platform.',
-
     // Sync error: if someone tries to make the same platform and model the source and target
     'sync_error_platforms' => 'Source and Target Platform cannot be the same with the same model.',
-
     // The source model refers to the model that data is being transferred from
     'source_model' => 'Source Model',
-
     // The target model refers to the model that data is being transferred to
     'target_model' => 'Target Model',
-
     // Use relations from the model to platform
     'use_platform_relations' => 'Use Platform Relations',
-
     // If exists refers to the action to take if the record already exists
     'if_exists' => 'If exists',
-
     // Filter ids refers to the ids to filter when syncing
     'filter_ids' => 'Filter ids',
-
     // Ignore ids refers to the ids to ignore when syncing
     'ignore_ids' => 'Ignore ids',
-
     // Sync all records refers to syncing all records
     'sync_all_records' => 'Sync all Records',
-
     // Sync only ids refers to syncing only the ids
     'sync_only_ids' => 'Sync only ids',
-
     // Sync all fields refers to syncing all fields
     'sync_all_fields' => 'Sync all Fields',
-
     // Sync only fields defined in the field mappings
     'field_mappings' => 'Field Mappings',
-
     // Use a transformer class to transform the data when syncing
     'use_transformer_class' => 'Use Transformer Class',
-
     // When was the last time the data was synced
     'last_sync' => 'Last Sync',
-
     // Source Platform and Model refers to the source of the data being transferred
     'source_platform_and_model' => 'Source Platform and Model',
-
     // Target Platform and Model refers to the target of the data being transferred
     'target_platform_and_model' => 'Target Platform and Model',
-
     // Syncable ID refers to the ID of the record being synced
     'syncable_id' => 'Syncable ID',
-
     // Syncable Type refers to the type of record being synced
     'syncable_type' => 'Syncable Type',
-
     // API Token refers to the token used to authenticate the API
     'api_token' => 'Api Token',
-
     // Generate Token refers to the action to generate a new token
     'generate_token' => 'Generate Token',
-
     // Locked means the platform or model is locked and cannot be synced
     'locked' => 'Locked',
-
     // Lock Reason refers to the reason the platform or model is locked
     'lock_reason' => 'Lock Reason',
-
 ];
