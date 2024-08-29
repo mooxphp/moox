a8cddc660496008a4c35592cf7b3437e151f7f1e
Kim Speer <93331309+Kim-the-Diamond@users.noreply.github.com>
2024-08-29 14:10:44 +0200

    changed core config and Serviceprovider (#700)
    * changed core config and Serviceprovider
    
    * Fix styling
    
    * Update core.php
    
    * wip tests
    
    ---------
    
    Co-authored-by: Kim-the-Diamond <Kim-the-Diamond@users.noreply.github.com>

--- packages/core/config/core.php
+++ packages/core/config/core.php
@@ -29,24 +29,344 @@ return [
     */
 
     'packages' => [
-        'audit' => 'Moox Audit',
-        'builder' => 'Moox Builder',
-        'core' => 'Moox Core',
-        'expiry' => 'Moox Expiry',
-        'jobs' => 'Moox Jobs',
-        'login-link' => 'Moox Login Link',
-        'notifications' => 'Moox Notifications',
-        'page' => 'Moox Page',
-        'passkey' => 'Moox Passkey',
-        'permission' => 'Moox Permission',
-        'press' => 'Moox Press',
-        'press-wiki' => 'Moox Press Wiki',
-        'security' => 'Moox Security',
-        'sync' => 'Moox Sync',
-        'training' => 'Moox Trainings',
-        'user' => 'Moox User',
-        'user-device' => 'Moox User Device',
-        'user-session' => 'Moox User Session',
+        'audit' => [
+            'package' => 'Moox Audit',
+            'models' => [
+                'Audit' => [
+                    'api' => [
+                        'Index' => '',
+                        'Show' => '',
+                        'Update' => '',
+                        'Delete' => '',
+                    ],
+                ],
+            ],
+        ],
+        'builder' => [
+            'package' => 'Moox Builder',
+            'models' => [
+                'Builder' => [
+                    'api' => [
+                        'Index' => '',
+                        'Show' => '',
+                        'Update' => '',
+                        'Delete' => '',
+                    ],
+                ],
+            ],
+        ],
+        'core' => [
+            'package' => 'Moox Core',
+            'models' => [],
+        ],
+        'expiry' => [
+            'package' => 'Moox Expiry',
+            'models' => [
+                'Expiry' => [
+                    'api' => [
+                        'Index' => '',
+                        'Show' => '',
+                        'Update' => '',
+                        'Delete' => '',
+                    ],
+                ],
+            ],
+        ],
+        'jobs' => [
+            'package' => 'Moox Jobs',
+            'models' => [
+                'Job' => [
+                    'api' => [
+                        'Index' => '',
+                        'Show' => '',
+                        'Update' => '',
+                        'Delete' => '',
+                    ],
+                ],
+                'FailedJob' => [
+                    'api' => [
+                        'Index' => '',
+                        'Show' => '',
+                        'Update' => '',
+                        'Delete' => '',
+                    ],
+                ],
+                'JobBatch' => [
+                    'api' => [
+                        'Index' => '',
+                        'Show' => '',
+                        'Update' => '',
+                        'Delete' => '',
+                    ],
+                ],
+                'JobManager' => [
+                    'api' => [
+                        'Index' => '',
+                        'Show' => '',
+                        'Update' => '',
+                        'Delete' => '',
+                    ],
+                ],
+            ],
+        ],
+        'login-link' => [
+            'package' => 'Moox Login Link',
+            'models' => [
+                'LoginLink' => [
+                    'api' => [
+                        'Index' => '',
+                        'Show' => '',
+                        'Update' => '',
+                        'Delete' => '',
+                    ],
+                ],
+            ],
+        ],
+        'notifications' => [
+            'package' => 'Moox Notifications',
+            'models' => [
+                'Notification' => [
+                    'api' => [
+                        'Index' => '',
+                        'Show' => '',
+                        'Update' => '',
+                        'Delete' => '',
+                    ],
+                ],
+            ],
+        ],
+        'page' => [
+            'package' => 'Moox Page',
+            'models' => [
+                'Page' => [
+                    'api' => [
+                        'Index' => '',
+                        'Show' => '',
+                        'Update' => '',
+                        'Delete' => '',
+                    ],
+                ],
+            ],
+        ],
+        'passkey' => [
+            'package' => 'Moox Passkey',
+            'models' => [
+                'Passkey' => [
+                    'api' => [
+                        'Index' => '',
+                        'Show' => '',
+                        'Update' => '',
+                        'Delete' => '',
+                    ],
+                ],
+            ],
+        ],
+        'press' => [
+            'package' => 'Moox Press',
+            'models' => [
+                'WpComment' => [
+                    'api' => [
+                        'Index' => '',
+                        'Show' => '',
+                        'Update' => '',
+                        'Delete' => '',
+                    ],
+                ],
+                'WpCommentMeta' => [
+                    'api' => [
+                        'Index' => '',
+                        'Show' => '',
+                        'Update' => '',
+                        'Delete' => '',
+                    ],
+                ],
+                'WpOption' => [
+                    'api' => [
+                        'Index' => '',
+                        'Show' => '',
+                        'Update' => '',
+                        'Delete' => '',
+                    ],
+                ],
+                'WpPost' => [
+                    'api' => [
+                        'Index' => '',
+                        'Show' => '',
+                        'Update' => '',
+                        'Delete' => '',
+                    ],
+                ],
+                'WpPostMeta' => [
+                    'api' => [
+                        'Index' => '',
+                        'Show' => '',
+                        'Update' => '',
+                        'Delete' => '',
+                    ],
+                ],
+                'WpTerm' => [
+                    'api' => [
+                        'Index' => '',
+                        'Show' => '',
+                        'Update' => '',
+                        'Delete' => '',
+                    ],
+                ],
+                'WpTermMeta' => [
+                    'api' => [
+                        'Index' => '',
+                        'Show' => '',
+                        'Update' => '',
+                        'Delete' => '',
+                    ],
+                ],
+                'WpTermRelationship' => [
+                    'api' => [
+                        'Index' => '',
+                        'Show' => '',
+                        'Update' => '',
+                        'Delete' => '',
+                    ],
+                ],
+                'WpTermTaxonomy' => [
+                    'api' => [
+                        'Index' => '',
+                        'Show' => '',
+                        'Update' => '',
+                        'Delete' => '',
+                    ],
+                ],
+                'WpUser' => [
+                    'authenticatable' => true,
+                    'api' => [
+                        'Index' => '',
+                        'Show' => '',
+                        'Update' => '',
+                        'Delete' => '',
+                    ],
+                ],
+                'WpUserMeta' => [
+                    'api' => [
+                        'Index' => '',
+                        'Show' => '',
+                        'Update' => '',
+                        'Delete' => '',
+                    ],
+                ],
+            ],
+        ],
+
+        'security' => [
+            'package' => 'Moox Security',
+            'models' => [
+                'ResetPassword' => [
+                    'api' => [
+                        'Index' => '',
+                        'Show' => '',
+                        'Update' => '',
+                        'Delete' => '',
+                    ],
+                ],
+            ],
+        ],
+        'sync' => [
+            'package' => 'Moox Sync',
+            'models' => [
+                'Platform' => [
+                    'api' => [
+                        'Index' => '',
+                        'Show' => '',
+                        'Update' => '',
+                        'Delete' => '',
+                    ],
+                ],
+                'Sync' => [
+                    'api' => [
+                        'Index' => '',
+                        'Show' => '',
+                        'Update' => '',
+                        'Delete' => '',
+                    ],
+                ],
+            ],
+        ],
+        'training' => [
+            'package' => 'Moox Training',
+            'models' => [
+                'Training' => [
+                    'api' => [
+                        'Index' => '',
+                        'Show' => '',
+                        'Update' => '',
+                        'Delete' => '',
+                    ],
+                ],
+                'TrainingDate' => [
+                    'api' => [
+                        'Index' => '',
+                        'Show' => '',
+                        'Update' => '',
+                        'Delete' => '',
+                    ],
+                ],
+                'TrainingInvitation' => [
+                    'api' => [
+                        'Index' => '',
+                        'Show' => '',
+                        'Update' => '',
+                        'Delete' => '',
+                    ],
+                ],
+                'TrainingType' => [
+                    'api' => [
+                        'Index' => '',
+                        'Show' => '',
+                        'Update' => '',
+                        'Delete' => '',
+                    ],
+                ],
+            ],
+        ],
+        'user' => [
+            'package' => 'Moox User',
+            'models' => [
+                'User' => [
+                    'authenticatable' => true,
+                    'api' => [
+                        'Index' => '',
+                        'Show' => '',
+                        'Update' => '',
+                        'Delete' => '',
+                    ],
+                ],
+            ],
+        ],
+        'user-device' => [
+            'package' => 'Moox User Device',
+            'models' => [
+                'UserDevice' => [
+                    'api' => [
+                        'Index' => '',
+                        'Show' => '',
+                        'Update' => '',
+                        'Delete' => '',
+                    ],
+                ],
+            ],
+        ],
+        'user-session' => [
+            'package' => 'Moox User Session',
+            'models' => [
+                'UserSession' => [
+                    'api' => [
+                        'Index' => '',
+                        'Show' => '',
+                        'Update' => '',
+                        'Delete' => '',
+                    ],
+                ],
+            ],
+        ],
     ],
 
     /*
