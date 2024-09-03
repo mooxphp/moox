a282802697dcf9ce11650988772c182d750f1821
Alf Drollinger <alf@drollinger.info>
2024-09-03 15:34:15 +0200

    Update api.php

--- packages/jobs/routes/api.php
+++ packages/jobs/routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Moox\Jobs\HttpSharedHosting\QueueWorker;
+use Moox\Jobs\Http\SharedHosting\QueueWorker;
 
 if (config('core.shared_hosting.enabled')) {
     Route::get('/queue/work', QueueWorker::class);
