049cd5bd1bd6c749a56a24b88824139097256917
Kim-the-Diamond <Kim-the-Diamond@users.noreply.github.com>
2024-09-09 09:43:53 +0000

    Fix styling

--- packages/user/src/Services/Login.php
+++ packages/user/src/Services/Login.php
@@ -2,26 +2,26 @@
 
 namespace Moox\User\Services;
 
-use Filament\Forms\Form;
-use Jenssegers\Agent\Agent;
-use Filament\Facades\Filament;
-use Filament\Pages\SimplePage;
+use DanHarrin\LivewireRateLimiting\Exceptions\TooManyRequestsException;
+use DanHarrin\LivewireRateLimiting\WithRateLimiting;
 use Filament\Actions\ActionGroup;
-use Illuminate\Routing\Redirector;
-use Illuminate\Support\HtmlString;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Http\RedirectResponse;
-use Illuminate\Support\Facades\Blade;
+use Filament\Facades\Filament;
 use Filament\Forms\Components\Checkbox;
 use Filament\Forms\Components\Component;
 use Filament\Forms\Components\TextInput;
-use Filament\Notifications\Notification;
-use Illuminate\Contracts\Support\Htmlable;
-use Illuminate\Validation\ValidationException;
-use DanHarrin\LivewireRateLimiting\WithRateLimiting;
-use Filament\Pages\Concerns\InteractsWithFormActions;
+use Filament\Forms\Form;
 use Filament\Http\Responses\Auth\Contracts\LoginResponse;
-use DanHarrin\LivewireRateLimiting\Exceptions\TooManyRequestsException;
+use Filament\Notifications\Notification;
+use Filament\Pages\Concerns\InteractsWithFormActions;
+use Filament\Pages\SimplePage;
+use Illuminate\Contracts\Support\Htmlable;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Routing\Redirector;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\HtmlString;
+use Illuminate\Validation\ValidationException;
+use Jenssegers\Agent\Agent;
 
 /**
  * @property Form $form
