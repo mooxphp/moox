19429844933e4f9021d9832204bb7af7f4ac196b
gallegonovato <fran-carro@hotmail.es>
2024-09-10 13:42:42 +0000

    Translated using Weblate (Spanish)
    Currently translated at 100.0% (5 of 5 strings)
    
    Translation: Moox/Notifications
    Translate-URL: https://hosted.weblate.org/projects/moox/notifications/es/

--- packages/core/resources/lang/es/notifications.php
+++ packages/core/resources/lang/es/notifications.php
@@ -1,10 +1,8 @@
 <?php
-
 return [
-
     'notification' => 'Notification',
     'notifications' => 'Notifications',
     'notifiable_type' => 'Notifiable_type',
     'notifiable_id' => 'Notifiable_id',
-
+    'hello' => 'Hola',
 ];
