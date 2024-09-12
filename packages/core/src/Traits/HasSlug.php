c097644b2df4533b7fd52e07059edfcb42a5ee0f
Alf Drollinger <alf@drollinger.info>
2024-09-12 11:42:52 +0200

    Update HasSlug.php

--- packages/core/src/Traits/HasSlug.php
+++ packages/core/src/Traits/HasSlug.php
@@ -3,39 +3,27 @@
 namespace Moox\Core\Traits;
 
 use Illuminate\Database\Eloquent\Model;
-use Spatie\Sluggable\HasSlug as SpatieHasSlug;
-use Spatie\Sluggable\SlugOptions;
+use Illuminate\Support\Str;
 
 trait HasSlug
 {
-    use SpatieHasSlug;
-
     public static function bootHasSlug()
     {
         static::saving(function (Model $model) {
             if (! $model->exists && empty($model->getAttribute('slug'))) {
-                $model->setAttribute('slug', static::generateUniqueSlugStatic($model->getAttribute('title')));
+                $model->setAttribute('slug', static::generateUniqueSlug($model->getAttribute('title')));
             }
         });
     }
 
-    public function getSlugOptions(): SlugOptions
-    {
-        return SlugOptions::create()
-            ->generateSlugsFrom('title')
-            ->saveSlugsTo('slug')
-            ->slugsShouldBeNoLongerThan(255)
-            ->doNotGenerateSlugsOnUpdate();
-    }
-
     public function generateSlug()
     {
-        $this->slug = $this->generateUniqueSlug($this->title);
+        $this->slug = static::generateUniqueSlug($this->title);
     }
 
     protected static function generateUniqueSlug($value)
     {
-        $slug = \Str::slug($value);
+        $slug = Str::slug($value);
         $originalSlug = $slug;
         $count = 1;
 
@@ -44,13 +32,6 @@ trait HasSlug
         }
 
         return $slug;
-    }
-
-    protected function slugExists($slug)
-    {
-        return static::where('slug', $slug)
-            ->where('id', '!=', $this->id ?? null)
-            ->exists();
     }
 
     public static function generateUniqueSlugStatic($value): string
