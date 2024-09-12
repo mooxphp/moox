39e7f47b7410c70a1002a29f06e4af877f2d09eb
Alf Drollinger <alf@drollinger.info>
2024-09-12 11:44:01 +0200

    Revert "Update HasSlug.php"
    This reverts commit fc65ebda5317f17f1a65e444d6d373bc9b23c2f5.

--- packages/core/src/Traits/HasSlug.php
+++ packages/core/src/Traits/HasSlug.php
@@ -3,27 +3,39 @@
 namespace Moox\Core\Traits;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Str;
+use Spatie\Sluggable\HasSlug as SpatieHasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 trait HasSlug
 {
+    use SpatieHasSlug;
+
     public static function bootHasSlug()
     {
         static::saving(function (Model $model) {
             if (! $model->exists && empty($model->getAttribute('slug'))) {
-                $model->setAttribute('slug', static::generateUniqueSlug($model->getAttribute('title')));
+                $model->setAttribute('slug', static::generateUniqueSlugStatic($model->getAttribute('title')));
             }
         });
     }
 
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('title')
+            ->saveSlugsTo('slug')
+            ->slugsShouldBeNoLongerThan(255)
+            ->doNotGenerateSlugsOnUpdate();
+    }
+
     public function generateSlug()
     {
-        $this->slug = static::generateUniqueSlug($this->title);
+        $this->slug = $this->generateUniqueSlug($this->title);
     }
 
     protected static function generateUniqueSlug($value)
     {
-        $slug = Str::slug($value);
+        $slug = \Str::slug($value);
         $originalSlug = $slug;
         $count = 1;
 
@@ -32,6 +44,13 @@ trait HasSlug
         }
 
         return $slug;
+    }
+
+    protected function slugExists($slug)
+    {
+        return static::where('slug', $slug)
+            ->where('id', '!=', $this->id ?? null)
+            ->exists();
     }
 
     public static function generateUniqueSlugStatic($value): string
