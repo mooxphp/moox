83e34cfb46654030b3c7fa4d2e21110a93680bec
Alf Drollinger <alf@drollinger.info>
2024-10-23 11:36:12 +0200

    Taxonomies in item list

--- packages/builder/src/Resources/ItemResource/Pages/ViewItem.php
+++ packages/builder/src/Resources/ItemResource/Pages/ViewItem.php
@@ -15,15 +15,31 @@ class ViewItem extends ViewRecord
 
     protected static string $resource = ItemResource::class;
 
-    public function mount($record = null): void
+    public function mount($record): void
     {
-        parent::mount($record);
+        $this->record = $this->resolveRecord($record);
+
+        $this->authorizeAccess();
+
+        $this->fillForm();
     }
 
-    protected function getHeaderActions(): array
+    protected function mutateFormDataBeforeFill(array $data): array
     {
-        return [
-        ];
+        return $this->mutateFormDataBeforeFillWithTaxonomies($data);
+    }
+
+    protected function fillForm(): void
+    {
+        $this->callHook('beforeFill');
+
+        $data = $this->record->toArray();
+
+        $data = $this->mutateFormDataBeforeFill($data);
+
+        $this->form->fill($data);
+
+        $this->callHook('afterFill');
     }
 
     public function getTitle(): string
@@ -39,5 +55,38 @@ class ViewItem extends ViewRecord
     private function isRecordTrashed(): bool
     {
         return $this->record instanceof Model && method_exists($this->record, 'trashed') && $this->record->trashed();
+    }
+
+    public function getRelationManagers(): array
+    {
+        return [];
+    }
+
+    protected function resolveRecord($key): Model
+    {
+        $model = static::getResource()::getModel();
+
+        $record = $model::findOrFail($key);
+
+        return $record;
+    }
+
+    protected function handleRecordUpdate(Model $record, array $data): Model
+    {
+        $record->update($data);
+
+        $this->handleTaxonomies();
+
+        return $record;
+    }
+
+    protected function getHeaderActions(): array
+    {
+        return [];
+    }
+
+    public function getFormActions(): array
+    {
+        return [];
     }
 }
