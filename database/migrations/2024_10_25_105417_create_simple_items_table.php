3191b0962476688390f6ef98d7f63373fcafd601
adrolli <adrolli@users.noreply.github.com>
2024-10-25 11:35:44 +0000

    Fix styling

--- database/migrations/2024_10_25_105417_create_simple_items_table.php
+++ database/migrations/2024_10_25_105417_create_simple_items_table.php
@@ -25,7 +25,6 @@ return new class extends Migration
      * Reverse the migrations.
      */
     public function down(): void
-
     {
         Schema::dropIfExists('simple_items');
     }
--- database/migrations/2024_10_25_111504_create_simple_taxonomyables_table.php
+++ database/migrations/2024_10_25_111504_create_simple_taxonomyables_table.php
@@ -23,7 +23,6 @@ return new class extends Migration
      * Reverse the migrations.
      */
     public function down(): void
-
     {
         Schema::dropIfExists('simple_taxonomyables');
     }
