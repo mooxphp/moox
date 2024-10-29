af7527dac70be07afe1ce2173d2d01631d5f61f6
adrolli <adrolli@users.noreply.github.com>
2024-10-29 10:23:53 +0000

    Fix styling

--- database/migrations/2024_10_29_095653_create_blubs_table.php
+++ database/migrations/2024_10_29_095653_create_blubs_table.php
@@ -12,10 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('blubs', function (Blueprint $table) {
-            
             $table->id();
             $table->timestamps();
-        
+
             $table->string('title');
             $table->text('content');
             $table->$table->softDeletes();
