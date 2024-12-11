afc3839f282fb94eacbc096d4bd32802ab9a4085
Alf Drollinger <alf@drollinger.info>
2024-12-11 15:12:59 +0100

    Add Skeleton

---
+++ packages/skeleton/resources/lang/es/translations.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+
+    'skeleton' => 'Esqueleto',
+
+];
---
+++ packages/skeleton/src/SkeletonServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Moox\Skeleton;
+
+use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
+
+class SkeletonServiceProvider extends PackageServiceProvider
+{
+    public function configurePackage(Package $package): void
+    {
+        $package
+            ->name('skeleton')
+            ->hasConfigFile()
+            ->hasViews()
+            ->hasTranslations()
+            ->hasMigrations()
+            ->hasCommands();
+    }
+}
