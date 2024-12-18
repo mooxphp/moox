2d470000de3f150cd4f3fe8bf996426666a72444
gallegonovato <fran-carro@hotmail.es>
2024-12-18 17:37:13 +0000

    Translated using Weblate (Spanish)
    Currently translated at 100.0% (6 of 6 strings)
    
    Translation: Moox/Audit
    Translate-URL: https://hosted.weblate.org/projects/moox/audit/es/

--- packages/core/resources/lang/es/audit.php
+++ packages/core/resources/lang/es/audit.php
@@ -1,23 +1,15 @@
 <?php
-
 return [
-
     // Audit refers to Logging of changes in the system and the package name Moox Audit
     'audit' => 'Audit',
-
     // Audits refers to Logging of changes in the system and the package name Moox Audit
-    'audits' => 'Audits',
-
+    'audits' => 'Auditorias',
     // The causer type is the type of the user who caused the log entry
-    'causer_type' => 'Causer type',
-
+    'causer_type' => 'Causante',
     // The causer id is the id of the user who caused the log entry
-    'causer_id' => 'Causer id',
-
+    'causer_id' => 'Id del causante',
     // Batch uuid is the unique identifier of the batch of logs
-    'batch_uuid' => 'Batch uuid',
-
+    'batch_uuid' => 'Uuid del lote',
     // Log name refers to the name of the logfile
     'log_name' => 'Log name',
-
 ];
