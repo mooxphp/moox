7ee2e7b7a0e937d859ec42f731cb549aafc39d44
gallegonovato <fran-carro@hotmail.es>
2024-12-18 17:33:06 +0000

    Translated using Weblate (Spanish)
    Currently translated at 100.0% (31 of 31 strings)
    
    Translation: Moox/Expiry
    Translate-URL: https://hosted.weblate.org/projects/moox/expiry/es/

--- packages/core/resources/lang/es/expiry.php
+++ packages/core/resources/lang/es/expiry.php
@@ -3,16 +3,16 @@ return [
     // Expiry refers to the expiration date of a record and the package name Moox Expiry
     'expiry' => 'Expiry',
     // Expiries refers to the expiration date of a record and the package name Moox Expiry
-    'expiries' => 'Expiries',
+    'expiries' => 'Vencimientos',
     // No assignee refers to the absence of an assignee for an expired record
-    'no_assignee' => 'No assignee',
+    'no_assignee' => 'Sin procurador',
     // No expiry date refers to the absence of an expiry date for a record
     'no_expiry_date' => 'No expiry date',
     'update_expiries' => 'Actualizar vencimientos',
     'updating_started' => 'Actualización iniciada',
-    'expired_at' => 'Caducado',
+    'expired_at' => 'Pendiente',
     'cycle' => 'Ciclo',
-    'notifyUser' => 'Persona responsable',
+    'notifyUser' => 'Procurador',
     'expiry_job' => 'Tipo',
     'set_date_based_on_cycle' => 'Fijar la fecha en función del ciclo',
     'date_updated' => 'Fecha de caducidad actualizada correctamente',
@@ -29,6 +29,10 @@ return [
     'no_expiry_set' => 'Si no se fija ninguna fecha de caducidad, ésta deberá fijarse manualmente.',
     'expiry_in_future' => 'La fecha de caducidad debe estar en el futuro.',
     'processing_deadline' => 'Fecha límite de procesamiento',
-    'escalated_to' => 'Escalado a',
-    'escalated_at' => 'Escalado en',
+    'escalated_to' => 'Recordatorio para',
+    'escalated_at' => 'Recordatorios',
+    'review_entries' => 'Por favor, revise estas entradas lo antes posible ',
+    'escalated_entries_in_expiry_dashboard' => 'Entradas escaladas en el panel de los vencimientos',
+    'following_escalated_entries' => 'Se escalan las siguientes entradas',
+    'expiry_dashboard' => 'Panel de control de los vencimientos',
 ];
