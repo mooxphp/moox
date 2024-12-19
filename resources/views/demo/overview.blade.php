1aa2fb58f576c62b25e7fa7c58283dd0eb462464
Kim-the-Diamond <kim.speer@web.de>
2024-12-19 10:27:02 +0100

    Update overview.blade.php

--- resources/views/demo/overview.blade.php
+++ resources/views/demo/overview.blade.php
@@ -2,12 +2,12 @@
 
 @section('content')
 
-            <p class="m-12 leading-6 text-center">
-                Moox is currently under active development.<br>
-                Our demo will be available shortly.<br>
-                <br>
-                Curiuos? Clone our <a href="https://github.com/mooxphp/moox">GitHub repository</a>
-                and run the demo locally.<br>
-            </p>
+    <p class="m-12 leading-6 text-center">
+        Moox is currently under active development.<br>
+        Our demo will be available shortly.<br>
+        <br>
+        Curious? Clone our <a href="https://github.com/mooxphp/moox">GitHub repository</a>
+        and run the demo locally.<br>
+    </p>
 
 @stop
