d8c607f17070b25d746a7caeec425248991b7765
Kim-the-Diamond <kim.speer@web.de>
2024-12-19 08:16:08 +0100

    fix skeleton

--- packages/skeleton/tests/TestCase.php
+++ packages/skeleton/tests/TestCase.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace VendorName\Skeleton\Tests;
+namespace Moox\Skeleton\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Moox\Skeleton\SkeletonServiceProvider;
@@ -13,7 +13,7 @@ class TestCase extends Orchestra
         parent::setUp();
 
         Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'VendorName\\Skeleton\\Database\\Factories\\'.class_basename($modelName).'Factory'
+            fn(string $modelName) => 'VendorName\\Skeleton\\Database\\Factories\\' . class_basename($modelName) . 'Factory'
         );
     }
 
