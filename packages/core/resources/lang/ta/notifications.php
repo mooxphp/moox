9f431eb1e3d1cba9eaa6db23250e8e311e82ff95
தமிழ்நேரம் <anishprabu.t@gmail.com>
2025-01-18 20:08:39 +0000

    Translated using Weblate (Tamil)
    Currently translated at 100.0% (5 of 5 strings)
    
    Translation: Moox/Notifications
    Translate-URL: https://hosted.weblate.org/projects/moox/notifications/ta/

--- packages/core/resources/lang/ta/notifications.php
+++ packages/core/resources/lang/ta/notifications.php
@@ -1,8 +1,8 @@
 <?php
 return [
-    'notification' => '',
-    'notifications' => '',
-    'notifiable_type' => '',
-    'notifiable_id' => '',
-    'hello' => '',
+    'notification' => 'அறிவிப்பு',
+    'notifications' => 'அறிவிப்புகள்',
+    'notifiable_type' => 'அறிவிக்கக்கூடிய_ வகை',
+    'notifiable_id' => 'அறிவிக்கக்கூடிய_ஐடி',
+    'hello' => 'வணக்கம்',
 ];
