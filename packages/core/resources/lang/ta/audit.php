cc5e95d48e6f7e5adc6da9d65c2c58adbb014ba2
தமிழ்நேரம் <anishprabu.t@gmail.com>
2025-01-18 20:07:01 +0000

    Translated using Weblate (Tamil)
    Currently translated at 100.0% (6 of 6 strings)
    
    Translation: Moox/Audit
    Translate-URL: https://hosted.weblate.org/projects/moox/audit/ta/

--- packages/core/resources/lang/ta/audit.php
+++ packages/core/resources/lang/ta/audit.php
@@ -1,15 +1,15 @@
 <?php
 return [
     // Audit refers to Logging of changes in the system and the package name Moox Audit
-    'audit' => '',
+    'audit' => 'தணிக்கை',
     // Audits refers to Logging of changes in the system and the package name Moox Audit
-    'audits' => '',
+    'audits' => 'தணிக்கை',
     // The causer type is the type of the user who caused the log entry
-    'causer_type' => '',
+    'causer_type' => 'அரட்டை',
     // The causer id is the id of the user who caused the log entry
-    'causer_id' => '',
+    'causer_id' => 'ஐடி காரணம்',
     // Batch uuid is the unique identifier of the batch of logs
-    'batch_uuid' => '',
+    'batch_uuid' => 'தொகுதி uuid',
     // Log name refers to the name of the logfile
-    'log_name' => '',
+    'log_name' => 'பதிவு பெயர்',
 ];
