fe17615eee6f3b3001d2168c0d178dd01e3f3c3c
தமிழ்நேரம் <anishprabu.t@gmail.com>
2025-01-19 04:07:26 +0000

    Translated using Weblate (Tamil)
    Currently translated at 100.0% (23 of 23 strings)
    
    Translation: Moox/Jobs
    Translate-URL: https://hosted.weblate.org/projects/moox/jobs/ta/

--- packages/core/resources/lang/ta/jobs.php
+++ packages/core/resources/lang/ta/jobs.php
@@ -1,49 +1,49 @@
 <?php
 return [
     // Job refers to Laravel's job queue, translate wisely
-    'job' => '',
+    'job' => 'பணி',
     // Jobs refers to Laravel's job queue, translate wisely
-    'jobs' => '',
+    'jobs' => 'வேலைகள்',
     // Queue refers to Laravel's job queue, translate wisely
-    'queue' => '',
+    'queue' => 'வரிசை',
     // Queues refers to Laravel's job queue, translate wisely
-    'queues' => '',
+    'queues' => 'வால்கள்',
     // Job refers to Laravel's job queue, translate wisely
-    'job_waiting' => '',
+    'job_waiting' => 'வேலை காத்திருப்பு',
     // Jobs refers to Laravel's job queue, translate wisely
-    'jobs_waiting' => '',
+    'jobs_waiting' => 'காத்திருக்கும் வேலைகள்',
     // Job refers to Laravel's job queue, translate wisely
-    'job_failed' => '',
+    'job_failed' => 'வேலை தோல்வியடைந்தது',
     // Jobs refers to Laravel's job queue, translate wisely
-    'jobs_failed' => '',
+    'jobs_failed' => 'வேலைகள் தோல்வியடைந்தன',
     // Jobs Batch refers to Laravel's job queue, translate wisely
-    'jobs_batch' => '',
+    'jobs_batch' => 'வேலை தொகுதி',
     // Jobs Batches refers to Laravel's job queue, translate wisely
-    'jobs_batches' => '',
+    'jobs_batches' => 'வேலைகள் தொகுதிகள்',
     // Jobs refers to Laravel's job queue, translate wisely
-    'waiting_jobs' => '',
+    'waiting_jobs' => 'காத்திருக்கும் மொத்த வேலைகள்',
     // Jobs refers to Laravel's job queue, translate wisely
-    'retry_all_failed_jobs' => '',
+    'retry_all_failed_jobs' => 'தோல்வியுற்ற அனைத்து வேலைகளையும் மீண்டும் முயற்சிக்கவும்',
     // Jobs refers to Laravel's job queue, translate wisely
-    'retry_all_failed_jobs_notification' => '',
+    'retry_all_failed_jobs_notification' => 'தோல்வியுற்ற அனைத்து வேலைகளும் மீண்டும் வரிசையில் தள்ளப்பட்டுள்ளன.',
     // Jobs refers to Laravel's job queue, translate wisely
-    'delete_all_failed_jobs' => '',
+    'delete_all_failed_jobs' => 'தோல்வியுற்ற அனைத்து வேலைகளையும் நீக்கியது',
     // Jobs refers to Laravel's job queue, translate wisely
-    'delete_all_failed_jobs_notification' => '',
+    'delete_all_failed_jobs_notification' => 'தோல்வியுற்ற அனைத்து வேலைகளும் அகற்றப்பட்டுள்ளன.',
     // Jobs refers to Laravel's job queue, translate wisely
-    'jobs_pushed_back_notification' => '',
+    'jobs_pushed_back_notification' => 'வேலைகள் மீண்டும் வரிசையில் தள்ளப்பட்டுள்ளன.',
     // Jobs refers to Laravel's job queue, translate wisely
-    'job_pushed_back_notification' => '',
+    'job_pushed_back_notification' => 'வரிசையில் மீண்டும் தள்ளப்பட்டுள்ளது.',
     // Jobs refers to Laravel's job queue, translate wisely
-    'failed_jobs' => '',
+    'failed_jobs' => 'தோல்வியுற்ற வேலைகள்',
     // Jobs refers to Laravel's job queue, translate wisely
-    'failed_job_id' => '',
+    'failed_job_id' => 'தோல்வியுற்ற வேலை ஐடி',
     // Jobs refers to Laravel's job queue, translate wisely
-    'pending_jobs' => '',
+    'pending_jobs' => 'வேலைகள் நிலுவையில் உள்ளன',
     // Jobs refers to Laravel's job queue, translate wisely
-    'total_jobs' => '',
+    'total_jobs' => 'மொத்த வேலைகள் செயல்படுத்தப்பட்டன',
     // Batches refers to Laravel's job queue, translate wisely
-    'prune_batches' => '',
+    'prune_batches' => 'அனைத்து தொகுதிகளையும் கத்தரிக்கவும்',
     // Batches refers to Laravel's job queue, translate wisely
-    'prune_batches_notification' => '',
+    'prune_batches_notification' => 'அனைத்து தொகுதிகளும் கத்தரிக்கப்பட்டுள்ளன.',
 ];
