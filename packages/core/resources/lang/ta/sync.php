565b30edfeeeb5f44d74d035ab9eed25ab891b5c
தமிழ்நேரம் <anishprabu.t@gmail.com>
2025-01-19 04:07:56 +0000

    Translated using Weblate (Tamil)
    Currently translated at 100.0% (37 of 37 strings)
    
    Translation: Moox/Sync
    Translate-URL: https://hosted.weblate.org/projects/moox/sync/ta/

--- packages/core/resources/lang/ta/sync.php
+++ packages/core/resources/lang/ta/sync.php
@@ -1,77 +1,77 @@
 <?php
 return [
     // Sync refers to the package name Moox Sync, and data transfer between platforms
-    'sync' => '',
+    'sync' => 'ஒத்திசைவு',
     // Syncs refers to the package name Moox Sync, and data transfer between platforms
-    'syncs' => '',
+    'syncs' => 'ஒத்திசைவு',
     // Platform refers to the platform that data is being transferred from or to
-    'platform' => '',
+    'platform' => 'இயங்குதளம்',
     // Platforms refers to the platforms that data is being transferred from or to
-    'platforms' => '',
+    'platforms' => 'தளங்கள்',
     // Source Platform refers to the platform that the data is being transferred from
-    'source_platform' => '',
+    'source_platform' => 'மூல தளம்',
     // Target Platform refers to the platform that the data is being transferred to
-    'target_platform' => '',
+    'target_platform' => 'இலக்கு தளம்',
     // Target Platforms refers to the platform that the data is being transferred to
-    'target_platforms' => '',
+    'target_platforms' => 'இலக்கு தளங்கள்',
     // Source Platform refers to the platform that data is being transferred from
-    'source_platform_id' => '',
+    'source_platform_id' => 'மூல இயங்குதள ஐடி',
     // Target Platform refers to the platform that data is being transferred to
-    'target_platform_id' => '',
+    'target_platform_id' => 'இலக்கு இயங்குதள ஐடி',
     // Sync error refers to any error that occurs in the package Moox Sync
-    'sync_error' => '',
+    'sync_error' => 'பிழை',
     // Sync error: if someone tries to make multiple platforms the master platform
-    'sync_error_master' => '',
+    'sync_error_master' => 'ஒரு முதன்மை தளம் மட்டுமே இருக்க முடியும்.',
     // Sync error: if someone tries to make the same platform and model the source and target
-    'sync_error_platforms' => '',
+    'sync_error_platforms' => 'மூல மற்றும் இலக்கு தளம் ஒரே மாதிரியுடன் ஒரே மாதிரியாக இருக்க முடியாது.',
     // The source model refers to the model that data is being transferred from
-    'source_model' => '',
+    'source_model' => 'மூல மாதிரி',
     // The target model refers to the model that data is being transferred to
-    'target_model' => '',
+    'target_model' => 'இலக்கு மாதிரி',
     // Use relations from the model to platform
-    'use_platform_relations' => '',
+    'use_platform_relations' => 'இயங்குதள உறவுகளைப் பயன்படுத்துங்கள்',
     // If exists refers to the action to take if the record already exists
-    'if_exists' => '',
+    'if_exists' => 'இருந்தால்',
     // Filter ids refers to the ids to filter when syncing
-    'filter_ids' => '',
+    'filter_ids' => 'ஐடிஎச் வடிகட்டி',
     // Ignore ids refers to the ids to ignore when syncing
-    'ignore_ids' => '',
+    'ignore_ids' => 'ஐடிகளை புறக்கணிக்கவும்',
     // Sync all records refers to syncing all records
-    'sync_all_records' => '',
+    'sync_all_records' => 'எல்லா பதிவுகளையும் ஒத்திசைக்கவும்',
     // Sync only ids refers to syncing only the ids
-    'sync_only_ids' => '',
+    'sync_only_ids' => 'ஐடிகளை மட்டும் ஒத்திசைக்கவும்',
     // Sync all fields refers to syncing all fields
-    'sync_all_fields' => '',
+    'sync_all_fields' => 'அனைத்து புலங்களையும் ஒத்திசைக்கவும்',
     // Sync only fields defined in the field mappings
-    'field_mappings' => '',
+    'field_mappings' => 'புலம் மேப்பிங்ச்',
     // Use a transformer class to transform the data when syncing
-    'use_transformer_class' => '',
+    'use_transformer_class' => 'மின்மாற்றி வகுப்பைப் பயன்படுத்தவும்',
     // When was the last time the data was synced
-    'last_sync' => '',
+    'last_sync' => 'கடைசி ஒத்திசைவு',
     // Source Platform and Model refers to the source of the data being transferred
-    'source_platform_and_model' => '',
+    'source_platform_and_model' => 'மூல தளம் மற்றும் மாதிரி',
     // Target Platform and Model refers to the target of the data being transferred
-    'target_platform_and_model' => '',
+    'target_platform_and_model' => 'இலக்கு தளம் மற்றும் மாதிரி',
     // Syncable ID refers to the ID of the record being synced
-    'syncable_id' => '',
+    'syncable_id' => 'ஒத்திசைக்கக்கூடிய ஐடி',
     // Syncable Type refers to the type of record being synced
-    'syncable_type' => '',
+    'syncable_type' => 'ஒத்திசைக்கக்கூடிய வகை',
     // API Token refers to the token used to authenticate the API
-    'api_token' => '',
+    'api_token' => 'பநிஇ கிள்ளாக்கு',
     // Generate Token refers to the action to generate a new token
-    'generate_token' => '',
+    'generate_token' => 'கிள்ளாக்கை உருவாக்குங்கள்',
     // Locked means the platform or model is locked and cannot be synced
-    'locked' => '',
+    'locked' => 'பூட்டப்பட்டுள்ளது',
     // Lock Reason refers to the reason the platform or model is locked
-    'lock_reason' => '',
+    'lock_reason' => 'பூட்டு காரணம்',
     // Model Compatibility Warning
-    'model_compatibility_warning' => '',
+    'model_compatibility_warning' => 'மாதிரி பொருந்தக்கூடிய எச்சரிக்கை',
     // The selected models are not fully compatible
-    'models_are_not_fully_compatible' => '',
+    'models_are_not_fully_compatible' => 'தேர்ந்தெடுக்கப்பட்ட மாதிரிகள் முழுமையாக பொருந்தாது',
     // Missing columns
-    'missing_columns' => '',
+    'missing_columns' => 'நெடுவரிசைகளைக் காணவில்லை',
     // Extra columns
-    'extra_columns' => '',
+    'extra_columns' => 'கூடுதல் நெடுவரிசைகள்',
     // Please map fields manually
-    'please_map_fields_manually' => '',
+    'please_map_fields_manually' => 'தயவுசெய்து புலங்களை கைமுறையாக வரைபடமாக்கவும்',
 ];
