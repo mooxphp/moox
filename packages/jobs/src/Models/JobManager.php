bb15548b14e8c6d751132f5f6f88ef4949ef1eb6
Alf Drollinger <alf@drollinger.info>
2025-01-22 14:17:30 +0100

    Clean workbenchs

--- packages/jobs/src/Models/JobManager.php
+++ packages/jobs/src/Models/JobManager.php
@@ -17,7 +17,7 @@ class JobManager extends Model
     /**
      * The attributes that are mass assignable.
      *
-     * @var array<int, string>
+     * @var list<string>
      */
     protected $fillable = [
         'job_id',
--- packages/user-device/src/Models/UserDevice.php
+++ packages/user-device/src/Models/UserDevice.php
@@ -18,7 +18,7 @@ class UserDevice extends Model
     /**
      * The attributes that are mass assignable.
      *
-     * @var array<int, string>
+     * @var list<string>
      */
     protected $fillable = [
         'title',
