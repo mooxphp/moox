f841437f2071358a93d4031be758c13916f72914
Aziz <104441723+AzGasim@users.noreply.github.com>
2025-02-10 16:54:53 +0100

    w-i-p

--- packages/media/src/Forms/Components/MediaPicker.php
+++ packages/media/src/Forms/Components/MediaPicker.php
@@ -15,7 +15,7 @@ class MediaPicker extends SpatieMediaLibraryFileUpload
 
         $this->saveRelationshipsUsing(function (self $component, $state) {
             $record = $component->getRecord();
-            if (! $record) {
+            if (!$record) {
                 return;
             }
 
@@ -23,23 +23,56 @@ class MediaPicker extends SpatieMediaLibraryFileUpload
                 $state = reset($state);
             }
 
-            if (! $state) {
+            if (!$state) {
                 return;
             }
 
             $media = Media::find($state);
 
-            if (! $media) {
+            if (!$media) {
                 return;
             }
 
-            $media->update([
-                'model_id' => $record->id,
-                'model_type' => get_class($record),
-            ]);
+            if ($media->model_id && $media->model_type) {
+                if ($media->model_id != $record->id || $media->model_type != get_class($record)) {
+                    Media::create([
+                        'model_id' => $record->id,
+                        'model_type' => get_class($record),
+                        'original_model_id' => $media->model_id,
+                        'original_model_type' => $media->model_type,
+                        'collection_name' => $media->collection_name,
+                        'name' => $media->name,
+                        'file_name' => $media->file_name,
+                        'mime_type' => $media->mime_type,
+                        'disk' => $media->disk,
+                        'conversions_disk' => $media->conversions_disk,
+                        'size' => $media->size,
+                        'manipulations' => $media->manipulations,
+                        'custom_properties' => $media->custom_properties,
+                        'generated_conversions' => $media->generated_conversions,
+                        'responsive_images' => $media->responsive_images,
+                        'order_column' => $media->order_column,
 
-            $image = $media->getUrl();
-            $component->state($image);
+                    ]);
+                } else {
+                    $component->state($media->uuid);
+                    return;
+                }
+            } else {
+                $media->update([
+                    'model_id' => $record->id,
+                    'model_type' => get_class($record),
+                ]);
+            }
+
+            $component->state($media->uuid);
+
+            $statePath = $component->getStatePath();
+            $fieldName = last(explode('.', $statePath));
+
+            $record->{$fieldName} = $media->getUrl();
+
+            $record->save();
         });
     }
 }
--- packages/media/src/Http/Livewire/MediaUploader.php
+++ packages/media/src/Http/Livewire/MediaUploader.php
@@ -11,19 +11,19 @@ class MediaUploader extends Component
 {
     use WithFilePond;
 
-    public $file; // FilePond hochgeladene Datei
+    public $file;
 
-    public ?Model $model = null; // Das zugehörige Modell
+    public ?Model $model = null;
 
-    public ?int $modelId = null; // ID des Modells
+    public ?int $modelId = null;
 
-    public ?string $modelClass = null; // Typ des Modells
+    public ?string $modelClass = null;
 
-    public string $collection = 'default'; // Name der Medien-Sammlung
+    public string $collection = 'default';
 
     public function mount(?int $modelId = null, ?string $modelClass = null)
     {
-        // Initialisierung: Falls ein `modelId` und `modelClass` angegeben sind, das zugehörige Modell laden
+        
         if ($modelId && $modelClass) {
             $modelClass = str_replace('\\\\', '\\', $modelClass);
 
@@ -46,41 +46,35 @@ class MediaUploader extends Component
 
     public function updatedFile()
     {
-        // Keine Datei hochgeladen, Abbruch
+      
         if (! $this->file) {
             return;
         }
 
-        // Prüfen, ob ein Modell vorhanden ist
+       
         if (! $this->modelId || ! $this->modelClass) {
             throw new \Exception('Kein Modell angegeben für den Upload.');
         }
 
-        // Datei zur spezifischen Model-Sammlung hinzufügen
         /** @var \Spatie\MediaLibrary\MediaCollections\Models\Media $media */
         $fileAdder = app(FileAdderFactory::class)->create($this->model, $this->file);
 
         $media = $fileAdder->toMediaCollection($this->collection);
 
-        // Medien mit `original_model_type` und `original_model_id` initialisieren
-        $media->original_model_type = $this->modelClass; // Modell-Klasse
-        $media->original_model_id = $this->modelId; // Modell-ID
+        $media->original_model_type = $this->modelClass; 
+        $media->original_model_id = $this->modelId;
 
-        // Felder `model_id` und `model_type` explizit auf `null` setzen
         $media->model_id = null;
         $media->model_type = null;
 
-        // Speichern der geänderten Werte
         $media->save();
 
-        // Datei zurücksetzen und Event auslösen
         $this->reset('file');
         $this->dispatch('mediaUploaded');
     }
 
     public function render()
     {
-        // Rendert die MediaUploader-Komponente
         return view('media::livewire.media-uploader');
     }
 }
