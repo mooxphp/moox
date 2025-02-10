fc05f964c3b4bb6c78ab6168627bf87e3d8a7499
Alf Drollinger <alf@drollinger.info>
2025-02-10 22:52:40 +0100

    Type

--- packages/user-session/src/Models/UserSession.php
+++ packages/user-session/src/Models/UserSession.php
@@ -24,7 +24,7 @@ class UserSession extends Model
     /**
      * The attributes that are mass assignable.
      *
-     * @var array<int, string>
+     * @var list<string>
      */
     protected $fillable = [
         'id',
