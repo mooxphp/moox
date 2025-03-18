1319e2a15618a300f3aa31a376aa0b5f0d54203f
Aziz Gasim <104441723+AzGasim@users.noreply.github.com>
2025-03-18 15:24:19 +0100

    small fixes

--- packages/media/src/MediaServiceProvider.php
+++ packages/media/src/MediaServiceProvider.php
@@ -4,20 +4,21 @@ declare(strict_types=1);
 
 namespace Moox\Media;
 
-use Filament\Support\Assets\Css;
+use Livewire\Livewire;
+use Moox\Media\Models\Media;
 use Filament\Support\Assets\Js;
-use Filament\Support\Facades\FilamentAsset;
+use Filament\Support\Assets\Css;
+use Illuminate\Support\Facades\Gate;
+use Moox\Media\Policies\MediaPolicy;
+use Illuminate\Support\Facades\Blade;
+use Spatie\LaravelPackageTools\Package;
 use Filament\Support\Facades\FilamentView;
 use Filament\Tables\View\TablesRenderHook;
-use Illuminate\Support\Facades\Gate;
-use Livewire\Livewire;
-use Moox\Media\Http\Livewire\MediaPickerModal;
+use Filament\Support\Facades\FilamentAsset;
 use Moox\Media\Http\Livewire\MediaUploader;
-use Moox\Media\Models\Media;
-use Moox\Media\Policies\MediaPolicy;
-use Moox\Media\Resources\MediaResource\Pages\ListMedia;
-use Spatie\LaravelPackageTools\Package;
+use Moox\Media\Http\Livewire\MediaPickerModal;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
+use Moox\Media\Resources\MediaResource\Pages\ListMedia;
 
 class MediaServiceProvider extends PackageServiceProvider
 {
@@ -39,21 +40,22 @@ class MediaServiceProvider extends PackageServiceProvider
 
         Gate::policy(Media::class, MediaPolicy::class);
 
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'media');
+        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'media');
         Livewire::component('media-picker-modal', MediaPickerModal::class);
         Livewire::component('media-uploader', MediaUploader::class);
 
         $this->publishes([
-            __DIR__.'/../resources/dist/icons' => public_path('vendor/media/icons'),
+            __DIR__ . '/../resources/dist/icons' => public_path('vendor/media/icons'),
         ], 'media-icons');
 
         FilamentAsset::register([
             Js::make('filepond-js', asset('vendor/livewire-filepond/filepond.js')),
             // Css::make('filepond-css', asset('vendor/livewire-filepond/filepond.css')),
         ]);
+
         FilamentView::registerRenderHook(
             TablesRenderHook::TOOLBAR_TOGGLE_COLUMN_TRIGGER_BEFORE,
-            fn (): string => Blade::render('@include("localization::lang-selector")'),
+            fn(): string => Blade::render('@include("localization::lang-selector")'),
             scopes: ListMedia::class
         );
     }
