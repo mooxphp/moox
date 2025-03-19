0aacb06a80a049a97815299f6403b15630499115
Alf Drollinger <40421928+adrolli@users.noreply.github.com>
2025-03-19 21:46:04 +0100

    Horizon

--- packages/data/src/Console/Commands/ImportStaticDataCommand.php
+++ packages/data/src/Console/Commands/ImportStaticDataCommand.php
@@ -7,12 +7,22 @@ use Moox\Data\Jobs\ImportStaticDataJob;
 
 class ImportStaticDataCommand extends Command
 {
-    protected $signature = 'moox:data:import-static';
+    protected $signature = 'moox:data:import-static {--sync : Run the import synchronously}';
 
     protected $description = 'Import static data from REST Countries API';
 
     public function handle()
     {
-        ImportStaticDataJob::dispatch();
+        $this->info('Starting static data import...');
+
+        if ($this->option('sync')) {
+            $this->info('Running import synchronously...');
+            (new ImportStaticDataJob)->handle();
+            $this->info('Import completed!');
+        } else {
+            $this->info('Dispatching import job to queue...');
+            ImportStaticDataJob::dispatch();
+            $this->info('Job dispatched! Make sure your queue worker is running.');
+        }
     }
 }
