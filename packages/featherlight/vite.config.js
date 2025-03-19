248ab1f1b278925c4abef25de4b46ce7117ec726
Alf Drollinger <40421928+adrolli@users.noreply.github.com>
2025-03-19 11:35:06 +0100

    Featherlight wired

--- packages/featherlight/vite.config.js
+++ packages/featherlight/vite.config.js
@@ -1,33 +1,18 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
-import { resolve } from "path";
-import tailwindcss from "@tailwindcss/postcss";
-import autoprefixer from "autoprefixer";
 
 export default defineConfig({
-    root: resolve(__dirname),
-    base: "./",
-    build: {
-        outDir: resolve(__dirname, "resources/dist"),
-        manifest: true,
-    },
     plugins: [
         laravel({
-            input: [
-                resolve(__dirname, "resources/src/app.css"),
-                resolve(__dirname, "resources/src/app.js"),
-            ],
+            input: ["resources/src/app.css", "resources/src/app.js"],
             refresh: true,
         }),
     ],
-    css: {
-        postcss: {
-            plugins: [
-                tailwindcss({
-                    config: resolve(__dirname, "tailwind.config.cjs"),
-                }),
-                autoprefixer(),
-            ],
+    build: {
+        outDir: "resources/dist",
+        manifest: true,
+        rollupOptions: {
+            input: ["resources/src/app.css", "resources/src/app.js"],
         },
     },
 });
