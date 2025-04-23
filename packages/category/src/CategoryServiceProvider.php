f99941519815c12bddc5f8109c965beeabf703fd
Kim Speer <93331309+Kim-the-Diamond@users.noreply.github.com>
2025-04-23 14:28:26 +0200

    added Lang Select

--- packages/category/src/CategoryServiceProvider.php
+++ packages/category/src/CategoryServiceProvider.php
@@ -4,9 +4,13 @@ declare(strict_types=1);
 
 namespace Moox\Category;
 
-use Moox\Category\Commands\InstallCommand;
+use Illuminate\Support\Facades\Blade;
 use Spatie\LaravelPackageTools\Package;
+use Filament\Support\Facades\FilamentView;
+use Filament\Tables\View\TablesRenderHook;
+use Moox\Category\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
+use Moox\Category\Moox\Entities\Categories\Category\Pages\ListCategories;
 
 class CategoryServiceProvider extends PackageServiceProvider
 {
@@ -19,5 +23,14 @@ class CategoryServiceProvider extends PackageServiceProvider
             ->hasTranslations()
             ->hasMigrations(['create_categories_table', 'create_categorizables_table', 'create_category_translations_table'])
             ->hasCommand(InstallCommand::class);
+    }
+
+    public function packageBooted(): void
+    {
+        FilamentView::registerRenderHook(
+            TablesRenderHook::TOOLBAR_TOGGLE_COLUMN_TRIGGER_BEFORE,
+            fn (): string => Blade::render('@include("localization::lang-selector")'),
+            scopes: ListCategories::class
+        );
     }
 }
