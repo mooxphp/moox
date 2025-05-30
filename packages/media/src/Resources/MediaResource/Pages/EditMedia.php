ee1bf41977b9a06c84f9aaf21fd961c38fd3d042
Aziz Gasim <104441723+AzGasim@users.noreply.github.com>
2025-05-30 12:23:19 +0200

    basic docs

--- packages/media/src/Resources/MediaResource/Pages/EditMedia.php
+++ packages/media/src/Resources/MediaResource/Pages/EditMedia.php
@@ -6,17 +6,8 @@ use Filament\Resources\Pages\EditRecord;
 use Moox\Media\Resources\MediaCollectionResource;
 use Moox\Media\Models\Media;
 
-class EditMediaCollection extends EditRecord
+class EditMedia extends EditRecord
 {
-    protected static string $resource = MediaCollectionResource::class;
+    protected static string $resource = MediaResource::class;
 
-    protected function mutateFormDataBeforeSave(array $data): array
-    {
-        $oldName = $this->record->name;
-
-        Media::where('collection_name', $oldName)
-            ->update(['collection_name' => $data['name']]);
-
-        return $data;
-    }
 }
