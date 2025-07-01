dcb0a3266f743678d8532574854ce98a6a3f5b14
Aziz Gasim <104441723+AzGasim@users.noreply.github.com>
2025-07-01 09:16:32 +0200

    size and position of translation icon

--- packages/localization/resources/views/filament/tables/columns/translations.blade.php
+++ packages/localization/resources/views/filament/tables/columns/translations.blade.php
@@ -15,19 +15,20 @@
 @endphp
 <x-filament-forms::field-wrapper>
 
-    <div class="flex">
-        @foreach($visibleFlags as $index => $flag)
-            <div class="relative" style="margin-left: -{{ $index * 4 }}px">
-                <x-dynamic-component :component="$flag" class="w-6 h-6 rounded-full" />
-            </div>
-        @endforeach
-
-        @if($remainingFlags > 0)
-            <div class="relative" style="margin-left: -4px">
-                <div class="flex items-center justify-center w-6 h-6 text-sm font-bold text-black rounded-full">
-                    +{{ $remainingFlags }}
+    <div style="padding-left: 30%;">
+        <div>
+            @foreach($visibleFlags as $index => $flag)
+                <div>
+                    <x-dynamic-component :component="$flag" style="width: 20px; height: 20px;" />
                 </div>
-            </div>
-        @endif
-    </div>
+            @endforeach
+
+            @if($remainingFlags > 0)
+                <div class="relative" style="margin-left: -4px">
+                    <div class="flex items-center justify-center w-6 h-6 text-sm font-bold text-black rounded-full">
+                        +{{ $remainingFlags }}
+                    </div>
+                </div>
+            @endif
+        </div>
 </x-filament-forms::field-wrapper>
