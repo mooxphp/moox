3f0ceff4c4a3e925b1c488db74ac963af02d7f7c
Aziz Gasim <104441723+AzGasim@users.noreply.github.com>
2025-07-18 09:27:27 +0200

    Update TitleWithSlug NameSpaces

--- packages/core/src/Forms/TaxonomyCreateForm.php
+++ packages/core/src/Forms/TaxonomyCreateForm.php
@@ -2,7 +2,8 @@
 
 namespace Moox\Core\Forms;
 
-use Camya\Filament\Forms\Components\TitleWithSlugInput;
+use Moox\Slug\Forms\Components\TitleWithSlugInput;
+
 
 class TaxonomyCreateForm
 {
--- packages/tag/src/Forms/TaxonomyCreateForm.php
+++ packages/tag/src/Forms/TaxonomyCreateForm.php
@@ -2,12 +2,12 @@
 
 namespace Moox\Tag\Forms;
 
-use Camya\Filament\Forms\Components\TitleWithSlugInput;
-use Filament\Forms\Components\ColorPicker;
-use Filament\Forms\Components\FileUpload;
-use Filament\Forms\Components\MarkdownEditor;
-use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Components\Grid;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\ColorPicker;
+use Filament\Forms\Components\MarkdownEditor;
+use Moox\Slug\Forms\Components\TitleWithSlugInput;
 
 class TaxonomyCreateForm
 {
