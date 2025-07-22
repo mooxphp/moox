eb7fe4adfe9b1a9fc17238124af4eeb1c4efa728
Chris Ware <chris-ware@users.noreply.github.com>
2025-07-22 09:04:46 +0100

    fix: Remove hasViews from core and clipboard to prevent issues when caching views (#780)
    Co-authored-by: Chris Ware <chris.ware@morphsites.com>

--- packages/clipboard/src/ClipboardServiceProvider.php
+++ packages/clipboard/src/ClipboardServiceProvider.php
@@ -14,7 +14,6 @@ class ClipboardServiceProvider extends PackageServiceProvider
         $package
             ->name('clipboard')
             ->hasConfigFile()
-            ->hasViews()
             ->hasTranslations()
             ->hasCommands()
             ->hasAssets();
