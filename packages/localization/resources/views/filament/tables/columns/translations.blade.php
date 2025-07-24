ae970b131e325c2d76a30db8ae41064ac28240a7
Aziz Gasim <104441723+AzGasim@users.noreply.github.com>
2025-07-24 08:23:45 +0200

    stack flags

--- packages/localization/resources/views/filament/tables/columns/translations.blade.php
+++ packages/localization/resources/views/filament/tables/columns/translations.blade.php
@@ -12,23 +12,28 @@
         $visibleFlags = [];
         $remainingFlags = 0;
     }
+    // Für "überlappend nach vorne": Array umdrehen
+    $visibleFlags = array_reverse($visibleFlags);
 @endphp
 <x-filament-forms::field-wrapper>
-
-    <div style="padding-left: 30%;">
-        <div>
-            @foreach($visibleFlags as $index => $flag)
-                <div>
-                    <x-dynamic-component :component="$flag" style="width: 20px; height: 20px;" />
-                </div>
+    <div style="padding-left: 30%; position: relative; height: 28px; min-width: 66px;">
+        <div style="position: relative; height: 28px; min-width: 66px;">
+            @foreach($visibleFlags as $revIndex => $flag)
+                @php $index = count($visibleFlags) - 1 - $revIndex; @endphp
+                <span style="position: absolute; left: {{ $index * 18 }}px; z-index: {{ 10 + $index }};">
+                    <x-dynamic-component :component="$flag"
+                        style="width: 24px; height: 24px; border-radius: 50%; background: #fff;" />
+                </span>
             @endforeach
 
             @if($remainingFlags > 0)
-                <div class="relative" style="margin-left: -4px">
-                    <div class="flex items-center justify-center w-6 h-6 text-sm font-bold text-black rounded-full">
+                <span style="position: absolute; left: {{ count($visibleFlags) * 18 }}px; z-index: 1;">
+                    <div
+                        class="flex items-center justify-center w-6 h-6 text-sm font-bold text-black rounded-full bg-white">
                         +{{ $remainingFlags }}
                     </div>
-                </div>
+                </span>
             @endif
         </div>
+    </div>
 </x-filament-forms::field-wrapper>
