c3f4a199292511ccdae16318ea510dcd330fe2dd
AzGasim <104441723+AzGasim@users.noreply.github.com>
2025-08-05 14:16:31 +0000

    Fix styling

--- packages/category/src/Moox/Entities/Categories/Category/Resources/CategoryResource/Pages/ViewCategory.php
+++ packages/category/src/Moox/Entities/Categories/Category/Resources/CategoryResource/Pages/ViewCategory.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Moox\Category\Moox\Entities\Categories\Category\Resources\CategoryResource\Pages;
 
-use Filament\Actions\EditAction;
-use Filament\Actions\RestoreAction;
 use Illuminate\Database\Eloquent\Model;
 use Moox\Category\Moox\Entities\Categories\Category\CategoryResource;
 use Moox\Core\Entities\Items\Draft\Pages\BaseViewDraft;
@@ -15,13 +13,12 @@ class ViewCategory extends BaseViewDraft
 {
     protected static string $resource = CategoryResource::class;
 
-
     #[Override]
     public function getTitle(): string
     {
         $title = parent::getTitle();
         if ($this->isRecordTrashed()) {
-            $title = $title . ' - ' . __('core::core.deleted');
+            $title = $title.' - '.__('core::core.deleted');
         }
 
         return $title;
