2b1b4fbcc5556b5b01bcd4d6ee9979057068124a
Aziz Gasim <104441723+AzGasim@users.noreply.github.com>
2025-08-05 15:36:25 +0200

    Adjust TaxonomyCreateForm

--- packages/category/src/Moox/Entities/Categories/Category/Forms/TaxonomyCreateForm.php
+++ packages/category/src/Moox/Entities/Categories/Category/Forms/TaxonomyCreateForm.php
@@ -2,14 +2,15 @@
 
 namespace Moox\Category\Moox\Entities\Categories\Category\Forms;
 
-use CodeWithDennis\FilamentSelectTree\SelectTree;
-use Filament\Forms\Components\ColorPicker;
-use Filament\Forms\Components\FileUpload;
-use Filament\Forms\Components\MarkdownEditor;
-use Filament\Forms\Components\TextInput;
+use Moox\Category\Models\Category;
 use Filament\Schemas\Components\Grid;
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Validation\Rules\Unique;
+use Filament\Forms\Components\TextInput;
+use Illuminate\Database\Eloquent\Builder;
+use Filament\Forms\Components\ColorPicker;
+use Moox\Media\Forms\Components\MediaPicker;
+use Filament\Forms\Components\MarkdownEditor;
+use CodeWithDennis\FilamentSelectTree\SelectTree;
 use Moox\Slug\Forms\Components\TitleWithSlugInput;
 
 class TaxonomyCreateForm
@@ -20,6 +21,8 @@ class TaxonomyCreateForm
             TitleWithSlugInput::make(
                 fieldTitle: 'title',
                 fieldSlug: 'slug',
+                fieldPermalink: 'permalink',
+                urlPathEntityType: 'categories',
                 slugRuleUniqueParameters: [
                     'modifyRuleUsing' => function (Unique $rule, $record, $livewire) {
                         $locale = $livewire->lang;
@@ -34,14 +37,12 @@ class TaxonomyCreateForm
                         } else {
                             $rule->where('locale', $locale);
                         }
-
-                        return $rule;
                     },
                     'table' => 'category_translations',
                     'column' => 'slug',
                 ]
             ),
-            FileUpload::make('featured_image_url')
+            MediaPicker::make('featured_image_url')
                 ->label(__('core::core.featured_image_url')),
             MarkdownEditor::make('content')
                 ->label(__('core::core.content')),
@@ -50,12 +51,13 @@ class TaxonomyCreateForm
                     relationship: 'parent',
                     titleAttribute: 'title',
                     parentAttribute: 'parent_id',
-                    modifyQueryUsing: fn (Builder $query, $get) => $query->where('id', '!=', $get('id'))
+                    modifyQueryUsing: fn(Builder $query, $get) => $query->where('id', '!=', $get('id'))
                 )
                 ->label('Parent Category')
                 ->searchable()
-                ->disabledOptions(fn ($get): array => [$get('id')])
-                ->enableBranchNode(),
+                ->disabledOptions(fn($get): array => [$get('id')])
+                ->enableBranchNode()
+                ->visible(fn() => Category::count() > 0),
             Grid::make(2)
                 ->schema([
                     ColorPicker::make('color'),
