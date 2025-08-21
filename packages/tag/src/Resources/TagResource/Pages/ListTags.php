d02a272bdfbe41c82ff456c5f2218039647cd855
AzGasim <104441723+AzGasim@users.noreply.github.com>
2025-08-21 10:17:36 +0000

    Fix styling

--- packages/tag/src/Resources/TagResource/Pages/ListTags.php
+++ packages/tag/src/Resources/TagResource/Pages/ListTags.php
@@ -4,14 +4,10 @@ declare(strict_types=1);
 
 namespace Moox\Tag\Resources\TagResource\Pages;
 
-use Filament\Actions\Action;
-use Filament\Actions\CreateAction;
-use Filament\Notifications\Notification;
 use Moox\Core\Entities\Items\Draft\Pages\BaseListDrafts;
 use Moox\Core\Traits\Tabs\HasListPageTabs;
 use Moox\Tag\Models\Tag;
 use Moox\Tag\Resources\TagResource;
-use Override;
 
 class ListTags extends BaseListDrafts
 {
