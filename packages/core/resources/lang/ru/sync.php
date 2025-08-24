b3482907ae3a9ccd41726250e8cb02bd19154831
Yurt Page <yurtpage+weblate@gmail.com>
2025-08-24 21:52:40 +0200

    Added translation using Weblate (Russian)

---
+++ packages/core/resources/lang/ru/sync.php
@@ -0,0 +1,77 @@
+<?php
+return [
+    // Sync refers to the package name Moox Sync, and data transfer between platforms
+    'sync' => '',
+    // Syncs refers to the package name Moox Sync, and data transfer between platforms
+    'syncs' => '',
+    // Platform refers to the platform that data is being transferred from or to
+    'platform' => '',
+    // Platforms refers to the platforms that data is being transferred from or to
+    'platforms' => '',
+    // Source Platform refers to the platform that the data is being transferred from
+    'source_platform' => '',
+    // Target Platform refers to the platform that the data is being transferred to
+    'target_platform' => '',
+    // Target Platforms refers to the platform that the data is being transferred to
+    'target_platforms' => '',
+    // Source Platform refers to the platform that data is being transferred from
+    'source_platform_id' => '',
+    // Target Platform refers to the platform that data is being transferred to
+    'target_platform_id' => '',
+    // Sync error refers to any error that occurs in the package Moox Sync
+    'sync_error' => '',
+    // Sync error: if someone tries to make multiple platforms the master platform
+    'sync_error_master' => '',
+    // Sync error: if someone tries to make the same platform and model the source and target
+    'sync_error_platforms' => '',
+    // The source model refers to the model that data is being transferred from
+    'source_model' => '',
+    // The target model refers to the model that data is being transferred to
+    'target_model' => '',
+    // Use relations from the model to platform
+    'use_platform_relations' => '',
+    // If exists refers to the action to take if the record already exists
+    'if_exists' => '',
+    // Filter ids refers to the ids to filter when syncing
+    'filter_ids' => '',
+    // Ignore ids refers to the ids to ignore when syncing
+    'ignore_ids' => '',
+    // Sync all records refers to syncing all records
+    'sync_all_records' => '',
+    // Sync only ids refers to syncing only the ids
+    'sync_only_ids' => '',
+    // Sync all fields refers to syncing all fields
+    'sync_all_fields' => '',
+    // Sync only fields defined in the field mappings
+    'field_mappings' => '',
+    // Use a transformer class to transform the data when syncing
+    'use_transformer_class' => '',
+    // When was the last time the data was synced
+    'last_sync' => '',
+    // Source Platform and Model refers to the source of the data being transferred
+    'source_platform_and_model' => '',
+    // Target Platform and Model refers to the target of the data being transferred
+    'target_platform_and_model' => '',
+    // Syncable ID refers to the ID of the record being synced
+    'syncable_id' => '',
+    // Syncable Type refers to the type of record being synced
+    'syncable_type' => '',
+    // API Token refers to the token used to authenticate the API
+    'api_token' => '',
+    // Generate Token refers to the action to generate a new token
+    'generate_token' => '',
+    // Locked means the platform or model is locked and cannot be synced
+    'locked' => '',
+    // Lock Reason refers to the reason the platform or model is locked
+    'lock_reason' => '',
+    // Model Compatibility Warning
+    'model_compatibility_warning' => '',
+    // The selected models are not fully compatible
+    'models_are_not_fully_compatible' => '',
+    // Missing columns
+    'missing_columns' => '',
+    // Extra columns
+    'extra_columns' => '',
+    // Please map fields manually
+    'please_map_fields_manually' => '',
+];
