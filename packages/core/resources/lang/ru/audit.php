545f9a85b5a95120b3cf79e432e5e2a2333130f5
Yurt Page <yurtpage+weblate@gmail.com>
2025-08-24 21:55:22 +0200

    Translated using Weblate (Russian)
    Currently translated at 100.0% (6 of 6 strings)
    
    Translation: Moox/Audit
    Translate-URL: https://hosted.weblate.org/projects/moox/audit/ru/

--- packages/core/resources/lang/ru/audit.php
+++ packages/core/resources/lang/ru/audit.php
@@ -1,15 +1,15 @@
 <?php
 return [
     // Audit refers to Logging of changes in the system and the package name Moox Audit
-    'audit' => '',
+    'audit' => 'Аудит',
     // Audits refers to Logging of changes in the system and the package name Moox Audit
-    'audits' => '',
+    'audits' => 'Аудиты',
     // The causer type is the type of the user who caused the log entry
-    'causer_type' => '',
+    'causer_type' => 'Тип логирующего',
     // The causer id is the id of the user who caused the log entry
-    'causer_id' => '',
+    'causer_id' => 'ИД логирующего',
     // Batch uuid is the unique identifier of the batch of logs
-    'batch_uuid' => '',
+    'batch_uuid' => 'Пакета uuid',
     // Log name refers to the name of the logfile
-    'log_name' => '',
+    'log_name' => 'Название журнала',
 ];
