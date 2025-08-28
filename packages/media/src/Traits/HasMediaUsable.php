8a419e3b7d769631628e622ee9e207fb90b11152
Aziz Gasim <104441723+AzGasim@users.noreply.github.com>
2025-08-28 09:49:41 +0200

    MediaUsable softdeletes

--- packages/media/src/Traits/HasMediaUsable.php
+++ packages/media/src/Traits/HasMediaUsable.php
@@ -8,10 +8,20 @@ trait HasMediaUsable
 {
     protected static function bootHasMediaUsable()
     {
-        static::deleted(function ($model) {
-            MediaUsable::where('media_usable_id', $model->id)
-                ->where('media_usable_type', get_class($model))
-                ->delete();
+        static::deleting(function ($model) {
+            if (method_exists($model, 'isForceDeleting') && $model->isForceDeleting()) {
+                MediaUsable::where('media_usable_id', $model->id)
+                    ->where('media_usable_type', get_class($model))
+                    ->delete();
+                return;
+            }
+
+            if (!in_array('Illuminate\Database\Eloquent\SoftDeletes', class_uses_recursive($model))) {
+                MediaUsable::where('media_usable_id', $model->id)
+                    ->where('media_usable_type', get_class($model))
+                    ->delete();
+            }
+
         });
     }
 }
