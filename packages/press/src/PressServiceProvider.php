77d4abd31a3aaa4b9842ae03e154708d35ccec9e
Alf Drollinger <40421928+adrolli@users.noreply.github.com>
2025-10-08 21:45:24 +0200

    New Press: routing wip

--- packages/press/src/PressServiceProvider.php
+++ packages/press/src/PressServiceProvider.php
@@ -4,10 +4,7 @@ declare(strict_types=1);
 
 namespace Moox\Press;
 
-use Illuminate\Contracts\Http\Kernel as HttpKernel;
-use Illuminate\Foundation\Http\Kernel as BaseHttpKernel;
 use Moox\Press\Commands\InstallCommand;
-use Override;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
@@ -21,26 +18,6 @@ class PressServiceProvider extends PackageServiceProvider
             ->hasTranslations()
             ->hasCommands(
                 InstallCommand::class,
-            )
-            ->hasRoutes(['web']);
-    }
-
-    public function register(): void
-    {
-        parent::register();
-
-        $this->app->singleton(HttpKernel::class, function ($app) {
-            return new class($app, $app['router']) extends BaseHttpKernel {
-                public function handle($request)
-                {
-                    if (preg_match('#^/wp(-admin|-[^/]*\.php|/.*|$)#', $request->getRequestUri())) {
-                        require_once base_path('public/wp/index.php');
-                        exit;
-                    }
-
-                    return parent::handle($request);
-                }
-            };
-        });
+            );
     }
 }
