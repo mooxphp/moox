cfe9034104438f0dd1864f7cf978b7dbbb3cbda9
Kim Speer <93331309+Kim-the-Diamond@users.noreply.github.com>
2025-10-17 09:38:39 +0200

    fix z index translationcolumn

--- packages/localization/resources/views/filament/tables/columns/translations.blade.php
+++ packages/localization/resources/views/filament/tables/columns/translations.blade.php
@@ -39,22 +39,22 @@
     <div style="padding-left: 30%; position: relative; height: 28px; min-width: 66px;">
         <div style="position: relative; height: 28px; min-width: 66px;">
             @foreach($visibleFlags as $index => $flag)
-                    @php 
-                                                        $isTrashed = str_contains($flag, 'trashed');
-                        $flagComponent = str_replace(' trashed', '', $flag);
-                    @endphp
-                <span style="position: absolute; left: {{ $index * 18 }}px; z-index: {{ 10 + $index }};">
-                                <x-dynamic-component :component="$flagComponent"
-                                            style="width: 24px; height: 24px; border-radius: 50%; background: #fff;" />
-                            </span>
+                            @php 
+                                                                                    $isTrashed = str_contains($flag, 'trashed');
+                                $flagComponent = str_replace(' trashed', '', $flag);
+                            @endphp
+                <span style="position: absolute; left: {{ $index * 18 }}px; z-index: {{ 5 + $index }};">
+                                                <x-dynamic-component :component="$flagComponent"
+                                                                    style="width: 24px; height: 24px; border-radius: 50%; background: #fff;" />
+                                                </span>
             @endforeach
-    @if($remainingFlags > 0)
-        <span style="position: absolute; left: {{ (count($visibleFlags) * 18) + 8 }}px; z-index: 20;">
+@if($remainingFlags > 0)
+        <span style="position: absolute; left: {{ (count($visibleFlags) * 18) + 8 }}px; z-index: {{ 5 + $index }};">
             <div class="flex items-center justify-center w-6 h-6 text-sm font-bold text-black rounded-full bg-white border border-gray-300">
-                        +{{ $remainingFlags }}
-            </div>
-        </span>
-    @endif
+                            +{{ $remainingFlags }}
+        </div>
+    </span>
+@endif
         </div>
     </div>
 </x-filament-forms::field-wrapper>
