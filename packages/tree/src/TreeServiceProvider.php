02121f2e774f00f632bd4f658cd70d90793892ef
AzGasim <104441723+AzGasim@users.noreply.github.com>
2026-02-05 13:35:01 +0000

    Fix styling

--- packages/tree/src/TreeServiceProvider.php
+++ packages/tree/src/TreeServiceProvider.php
@@ -4,9 +4,6 @@ declare(strict_types=1);
 
 namespace Moox\Tree;
 
-use Filament\Support\Facades\FilamentView;
-use Filament\Tables\View\TablesRenderHook;
-use Illuminate\Support\Facades\Blade;
 use Moox\Core\MooxServiceProvider;
 use Spatie\LaravelPackageTools\Package;
 
