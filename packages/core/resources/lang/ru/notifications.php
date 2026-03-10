faa935e9e666a7f67464120dd0fdaf29f2fda232
Anonymous <noreply@weblate.org>
2026-03-10 22:26:56 +0100

    Translated using Weblate (Russian)
    Currently translated at 60.0% (3 of 5 strings)
    
    Translation: Moox/Notifications
    Translate-URL: https://hosted.weblate.org/projects/moox/notifications/ru/

--- packages/core/resources/lang/ru/notifications.php
+++ packages/core/resources/lang/ru/notifications.php
@@ -1,7 +1,8 @@
 <?php
-
 return [
     'notification' => 'Уведомление',
     'notifications' => 'Уведомления',
     'hello' => 'Привет',
+    'notifiable_type' => '',
+    'notifiable_id' => '',
 ];
