2b7492621cf5538a76a10cc5dc87858a1805b24b
Anonymous <noreply@weblate.org>
2026-03-10 22:27:10 +0100

    Translated using Weblate (Arabic)
    Currently translated at 12.9% (4 of 31 strings)
    
    Translation: Moox/Expiry
    Translate-URL: https://hosted.weblate.org/projects/moox/expiry/ar/

--- packages/core/resources/lang/ar/expiry.php
+++ packages/core/resources/lang/ar/expiry.php
@@ -1,17 +1,38 @@
 <?php
-
 return [
-
     // Expiry refers to the expiration date of a record and the package name Moox Expiry
     'expiry' => 'Expiry',
-
     // Expiries refers to the expiration date of a record and the package name Moox Expiry
     'expiries' => 'Expiries',
-
     // No assignee refers to the absence of an assignee for an expired record
     'no_assignee' => 'No assignee',
-
     // No expiry date refers to the absence of an expiry date for a record
     'no_expiry_date' => 'No expiry date',
-
+    'date_updated' => '',
+    'expired_at' => '',
+    'yearly' => '',
+    'following_escalated_entries' => '',
+    'expiry_dashboard' => '',
+    'set_new_expiry_date' => '',
+    'cycle' => '',
+    'expiry_job' => '',
+    'half_yearly' => '',
+    'weekly' => '',
+    'set_date_based_on_cycle' => '',
+    'save' => '',
+    'processing_deadline' => '',
+    'updating_started' => '',
+    'monthly' => '',
+    'notifyUser' => '',
+    'review_entries' => '',
+    'no_expiry_set' => '',
+    'update_expiries' => '',
+    'escalated_at' => '',
+    'escalated_entries_in_expiry_dashboard' => '',
+    'update_expiry_date' => '',
+    'no_fixed_cycle' => '',
+    'previous_expiry_date' => '',
+    'quarterly' => '',
+    'escalated_to' => '',
+    'expiry_in_future' => '',
 ];
