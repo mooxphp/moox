7da0918cdf9d7c874483f53d6eced07be6651ba6
adrolli <40421928+adrolli@users.noreply.github.com>
2026-05-18 13:47:42 +0000

    Fix styling

--- packages/press/stubs/public/wp/wp-content/db.php
+++ packages/press/stubs/public/wp/wp-content/db.php
@@ -1,8 +1,9 @@
 <?php
 
 declare(strict_types=1);
+use Moox\Press\WPDBProxy;
 
 require_once __DIR__.'/../../../vendor/moox/press/src/WPDBProxy.php';
 
 global $wpdb;
-$wpdb = new Moox\Press\WPDBProxy(DB_USER, DB_PASSWORD, DB_NAME, DB_HOST);
+$wpdb = new WPDBProxy(DB_USER, DB_PASSWORD, DB_NAME, DB_HOST);
