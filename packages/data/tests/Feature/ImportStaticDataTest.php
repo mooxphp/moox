861a7b64e69f80bf27284f85da09d13c02b6b3f6
jbagsik <jessica.bagsik@heco.de>
2026-07-08 10:59:14 +0200

    import Swiss German as a separate language from German

--- packages/data/tests/Feature/ImportStaticDataTest.php
+++ packages/data/tests/Feature/ImportStaticDataTest.php
@@ -68,3 +68,74 @@ test('import static data job imports countries using authenticated rest countrie
             && $request->hasHeader('Authorization', 'Bearer test-api-key');
     });
 });
+
+test('import static data job keeps german and swiss german as separate language records', function (): void {
+    Http::fake([
+        'api.restcountries.com/countries/v5*' => Http::response([
+            'data' => [
+                'objects' => [
+                    [
+                        'names' => ['common' => 'Germany', 'native' => [], 'alternates' => [], 'translations' => []],
+                        'codes' => ['alpha_2' => 'DE', 'alpha_3' => 'DEU'],
+                        'region' => 'Europe',
+                        'subregion' => 'Western Europe',
+                        'capitals' => [['name' => 'Berlin']],
+                        'population' => 83200000,
+                        'area' => ['kilometers' => 357114],
+                        'currencies' => [],
+                        'languages' => [['iso639_3' => 'deu', 'name' => 'German']],
+                        'timezones' => [],
+                        'calling_codes' => ['49'],
+                        'tlds' => ['.de'],
+                        'memberships' => [],
+                        'postal_code' => ['format' => null, 'regex' => null],
+                    ],
+                    [
+                        'names' => ['common' => 'Switzerland', 'native' => [], 'alternates' => [], 'translations' => []],
+                        'codes' => ['alpha_2' => 'CH', 'alpha_3' => 'CHE'],
+                        'region' => 'Europe',
+                        'subregion' => 'Western Europe',
+                        'capitals' => [['name' => 'Bern']],
+                        'population' => 8700000,
+                        'area' => ['kilometers' => 41284],
+                        'currencies' => [],
+                        'languages' => [
+                            ['iso639_3' => 'gsw', 'name' => 'Swiss German'],
+                            ['iso639_3' => 'deu', 'name' => 'German'],
+                        ],
+                        'timezones' => [],
+                        'calling_codes' => ['41'],
+                        'tlds' => ['.ch'],
+                        'memberships' => [],
+                        'postal_code' => ['format' => null, 'regex' => null],
+                    ],
+                ],
+                'meta' => [
+                    'total' => 2,
+                    'count' => 2,
+                    'limit' => 100,
+                    'offset' => 0,
+                    'more' => false,
+                ],
+            ],
+        ]),
+        'www.apicountries.com/*' => Http::response([
+            ['alpha2Code' => 'DE', 'languages' => [['iso639_1' => 'de', 'nativeName' => 'Deutsch']]],
+        ]),
+    ]);
+
+    (new ImportStaticDataJob)->handle();
+
+    $german = \Moox\Data\Models\StaticLanguage::query()->where('alpha2', 'de')->first();
+    $swissGerman = \Moox\Data\Models\StaticLanguage::query()->where('alpha2', 'gsw')->first();
+
+    expect($german)->not->toBeNull()
+        ->and($german->common_name)->toBe('German')
+        ->and($german->alpha3_b)->toBe('deu')
+        ->and($german->native_name)->toBe('Deutsch')
+        ->and($swissGerman)->not->toBeNull()
+        ->and($swissGerman->common_name)->toBe('Swiss German')
+        ->and($swissGerman->alpha3_b)->toBe('gsw')
+        ->and(\Moox\Data\Models\StaticLocale::query()->where('locale', 'de_CH')->value('name'))->toBe('German')
+        ->and(\Moox\Data\Models\StaticLocale::query()->where('locale', 'gsw_CH')->value('name'))->toBe('Swiss German');
+});
