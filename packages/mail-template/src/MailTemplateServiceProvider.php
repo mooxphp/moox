7a06ae2979660cfe70fb66c3befbd1c464c69da7
Aziz Gasim <104441723+AzGasim@users.noreply.github.com>
2026-08-31 17:12:57 +0200

    Fold mail-template subject into the create migration stub

--- packages/mail-template/src/MailTemplateServiceProvider.php
+++ packages/mail-template/src/MailTemplateServiceProvider.php
@@ -15,24 +15,6 @@ class MailTemplateServiceProvider extends MooxServiceProvider
             ->name('mail-template')
             ->hasConfigFile()
             ->hasTranslations()
-            ->hasMigrations([
-                'create_mail_templates_table',
-                'add_subject_to_mail_templates_table',
-            ])
-            ->runsMigrations();
-
-        $this->getMooxPackage()
-            ->title('Moox Mail Template')
-            ->released(false)
-            ->stability('dev')
-            ->category('mail')
-            ->usedFor([
-                'MJML mail templates with logo, footer, and Blade view links',
-            ]);
-    }
-
-    public function packageRegistered(): void
-    {
-        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+            ->hasMigrations('create_mail_templates_table');
     }
 }
